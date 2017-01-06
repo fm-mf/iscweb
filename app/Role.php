c153218ff2ecd05ceb87cdf2de9100350ad57d6d
Michal Kral <michal.kral@meetangee.com>
2017-01-06 06:20:47 +0100

    initial commit

---
+++ app/Role.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    protected $primaryKey = 'id_role';
+    public function users()
+    {
+        return $this->belongsToMany('\App\User', 'users_roles', 'id_role', 'id_user');
+    }
+}
