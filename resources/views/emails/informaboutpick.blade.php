1486007aad972d01a17c84554c13f75dc7d70172
= <=>
2017-01-21 04:49:26 +0100

    fix sent mail

--- resources/views/emails/informaboutpick.blade.php
+++ resources/views/emails/informaboutpick.blade.php
@@ -7,9 +7,15 @@
 <p>Hello there!<br>
 We are bringing great news – you have been paired with a Czech student.
 Your buddy should contact you soon and  provide you with more details.</p>
-
+<?php
+        if (isset($buddy->person->sex) && $buddy->person->sex) {
+            $s = $buddy->person->sex == 'M' ? 'his' : 'her';
+        } else {
+            $s = 'his/her';
+        }
+?>
 <p>If you want to, you can take the initiative and be the first one to start the conversation.<br>
-Here is {{ $buddy->person->sex == 'M' ? 'his' : 'her' }} profile: <br>
+Here is {{ $s }} profile: <br>
 {{ $buddy->person->first_name }} {{ $buddy->person->last_name }}, <br>
 {{ $buddy->person->user->email }}</p>
 
