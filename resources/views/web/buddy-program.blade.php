5c05917d7af2d2c9039034eb680c4b9d40d55bcd
Martin Průcha <prucma@gmail.com>
2017-01-21 01:12:45 +0100

    change URL::assets to assets

--- resources/views/web/buddy-program.blade.php
+++ resources/views/web/buddy-program.blade.php
@@ -20,18 +20,18 @@
         </div>
         <ul class="row list-unstyled info-buddy">
             <li class="col-sm-4">
-                <img src="{{ URL::asset('img/web/laptop.jpg') }}">
+                <img src="{{ asset('img/web/laptop.jpg') }}">
                 <p>Your buddy will contact you by email a couple of weeks before the semester starts.</p>
                 <!-- <p>Please, fill out your arrival information after you get access to our buddy system (will be sent to you by email).</p> -->
             </li>
             <li class="col-sm-4">
-                <img src="{{ URL::asset('img/web/airport-hug.jpg') }}">
+                <img src="{{ asset('img/web/airport-hug.jpg') }}">
                 <p>
                     Your buddy will pick you up at the airport (or station) and will help you with the accommodation in your dormitory.
                 </p>
             </li>
             <li class="col-sm-4">
-                <img src="{{ URL::asset('img/web/bridge.jpg') }}">
+                <img src="{{ asset('img/web/bridge.jpg') }}">
                 <p>
                     Not only will your buddy show you around, but he or she might also take you to his or her favorite pub and teach you how to drink Czech beer :)
                 </p>
@@ -43,7 +43,7 @@
         <div class="row">
             <div class="col-sm-8 col-sm-offset-2 link-buddies">
                 <p class="light-bg">Are you a <strong>Czech student</strong> wishing to take part in the Buddy Program? Then follow
-                    <a href="http://isc.cvut.cz/buddy"> <button type="button" class="btn btn-primary">THIS&nbsp;LINK</button></a>
+                    <a href="{{ url('/buddy') }}"> <button type="button" class="btn btn-primary">THIS&nbsp;LINK</button></a>
                     to find more information for Czech Buddies.</p>
 
             </div>
