8f1ec35c47b2f52a3ac8c44441419c69bb4d7c9a
= <=>
2017-01-21 02:28:11 +0100

    user timestamps

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
-    public $timestamps = false;
+    public $timestamps = true;
     protected $primaryKey = 'id_user';
 
     /**
