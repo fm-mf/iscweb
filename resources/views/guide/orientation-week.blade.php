a6297e6a99684720c23f09596ae4ea5498df06a4
Martin Průcha <prucma@gmail.com>
2017-01-30 14:07:56 +0100

    Update OW calender in Survival Guide

--- resources/views/guide/orientation-week.blade.php
+++ resources/views/guide/orientation-week.blade.php
@@ -21,16 +21,15 @@
 
     <!--The schedule of the Orientation Week will be announced soon. Stay tuned!-->
     <div class="row">
-        <h3>{{ $owFromTo }} (more information soon)</h3>
+        <h3>{{ $owFromTo }}</h3>
     </div>
 
-    <!--
     <div class="row">
-        <h3>DAY 1 - Monday <b>(September 26th)</b></h3>
+        <h3>DAY 1 - Monday <b>(February 13th)</b></h3>
     </div>
     <div class="row">
         <div class="col-lg-12">
-            Initial information meeting at <b>11:00</b> - 14:30, <b>Bethlehem Chapel (Betlémská kaple)</b>
+            Initial information meeting at <b>10:30</b> - 14:30, <b>Faculty of Civil Engineering, room D-1122</b>
             <ul>
                 <li>Official welcome</li>
                 <li>How to "survive" in Prague</li>
@@ -39,24 +38,26 @@
                 <li>ISC membership</li>
                 <li>Introduction to the Czech Culture Course</li>
                 <li>Life in the Czech Republic</li>
+                <li>How to enroll in courses</li>
+
             </ul>
         </div>
         <div class="col-lg-12">
-            Tandem party at <b>20:00, Radlická – kulturní sportovna</b>
+            Tandem Beer (more info soon)
             <ul>
-                <li>Enjoy the first party of the semester and get involved in our unique language learning program! Here you can meet a tandem partner to practice your language skills and have a beer.</li>
+                <li>Enjoy the first beer of the semester and get involved in our unique language learning program! Here you can meet a tandem partner to practice your language skills and have a drink.</li>
             </ul>
         </div>
     </div>
 
     <div class="row">
-        <h3>DAY 2 - Tuesday (September 27th)</h3>
+        <h3>DAY 2 - Tuesday (February 14th)</h3>
     </div>
     <div class="row">
         <div class="col-lg-12">
             In the morning - <strong>Registration</strong> at the faculties (for Erasmus and Exchange students).<br><b>You will need:</b>
             <ul>
-                <li>Study record book (green booklet) from the welcome pack</li>
+                <li><b>Study record book</b> (green booklet) from the Welcome Pack</li>
                 <li>Passport photo</li>
                 <li>Pen</li>
             </ul>
@@ -64,7 +65,7 @@
     </div>
     <div class="row">
         <div class="col-lg-12">
-            2nd information meeting at 15:00 - 17:30, Congress hall of the Masarykova dormitory
+            2nd information meeting at 14:00 - 16:30, Congress hall of the Masarykova dormitory
             <ul>
                 <li>ISC trip registrations (ESN card & cash needed)</li>
             </ul>
@@ -72,11 +73,11 @@
     </div>
 
     <div class="row">
-        <h3>DAY 3 - Wednesday (September 28th)</h3>
+        <h3>DAY 3 - Wednesday (February 15th)</h3>
     </div>
     <div class="row">
         <div class="col-lg-12">
-            Prague Discovery Game (PDG) at 13:00, Faculty of Electrical Engineering, room 256
+            Prague Discovery Game (PDG) at 13:30, Faculty of Electrical Engineering, room 256
             <ul>
                 <li>Great treasure hunt organized by ISC</li>
             </ul>
@@ -84,7 +85,7 @@
     </div>
     <div class="row">
         <div class="col-lg-12">
-            inteGREATion party at 21:00, P.M. Club
+            inteGREATion party in the evening at Storm Club
             <ul>
                 <li>Get to know each other at the first ISC inteGREAT party and have fun!</li>
             </ul>
@@ -92,7 +93,7 @@
     </div>
 
     <div class="row">
-        <h3>DAY 4-7 - Thursday to Sunday (September 29th – October 2nd) </h3>
+        <h3>DAY 4-7 - Thursday to Sunday (February 16th – 19th) </h3>
     </div>
     <div class="row">
         <div class="col-lg-12">
@@ -104,7 +105,7 @@
         </div>
     </div>
     <br>
-    -->
+
     <div class="row">
         <div class="col-md-12 bg-danger">Please keep in mind there are four types of students: Erasmus students,
             Exchange students, Erasmus Mundus students and Private (self-paying)
