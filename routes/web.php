c59fbbd1a479fa69ae331ffbd6c6f09b4094a3f3
Michal Kral <michal.kral@live.com>
2017-02-04 13:24:57 +0100

    buddy detail: layout fix

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::group(['middleware' => ['checkpartak', 'auth'], 'namespace' => 'Partak', 
 {
     Route::get('/', 'DashboardController@index');
     Route::get('/roles', 'RolesController@showSearchUsers');
-    Route::get('/users/buddy', 'UsersController@showBuddiesDashboard');
+    Route::get('/users/buddies', 'UsersController@showBuddiesDashboard');
 
     Route::get('/users/buddy/{id}', 'UsersController@showBuddyDetail');
     Route::get('/users/buddy/{id_buddy}/remove/{id_exStudent}', 'UsersController@removeExStudentFromBuddy');
