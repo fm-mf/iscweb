3f347f219349139311edf1892dca89b992ab955a
Michal Kral <michal.kral@live.com>
2017-02-04 13:24:57 +0100

    buddy detail: layout fix

--- resources/views/partak/layout/menu.blade.php
+++ resources/views/partak/layout/menu.blade.php
@@ -4,7 +4,7 @@
             <div class="collapse navbar-collapse navbar-main-collapse col-sm-9">
                 <ul class="nav navbar-nav admin-nav">
                     <li><a href="{{ url('partak/') }}" @if(Request::is('partak')) class="active" @endif>Dashboard</a></li>
-                    <li><a href="{{ url('partak/users/buddy') }}" @if(Request::is('partak/users/*')) class="active" @endif>Users</a></li>
+                    <li><a href="{{ url('partak/users/buddies') }}" @if(Request::is('partak/users/*')) class="active" @endif>Users</a></li>
                 </ul>
             </div>
         </div>
