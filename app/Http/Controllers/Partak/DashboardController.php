d6d9c3da27be3ad40b14024d4956f7536cbe2a78
Michal Kral <michal.kral@live.com>
2017-02-05 12:29:42 +0100

    fix partaknet

--- app/Http/Controllers/Partak/DashboardController.php
+++ app/Http/Controllers/Partak/DashboardController.php
@@ -11,8 +11,6 @@ class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        $martin = User::find(4);
-        dd('buddy: ' . $martin->isBuddy(). ', unverified buddy: ' . $martin->isUnverifiedBuddy());
         return view('partak.dashboard')->with(['user' => $request->user()]);
     }
 
