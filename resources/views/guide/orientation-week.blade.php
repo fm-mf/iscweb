6f900eaa7168d2dd40123623056626b6ab0c5bf8
Martin Průcha <prucma@gmail.com>
2017-02-13 21:00:46 +0100

    Guide - OW pdf document update

--- resources/views/guide/orientation-week.blade.php
+++ resources/views/guide/orientation-week.blade.php
@@ -52,12 +52,12 @@
         <h4>OW presentation Monday</h4>
 
             <div class="col-lg-12">
-                <a href="{{ asset('/files/OW_S2017/OW_S2017_Meeting_points_Registrations_at_faculties.pdf') }}">Registrations</a><br>
+                <a href="{{ asset('/files/OW_S2017/OW_S2017_Meeting_points_Registrations_at_faculties.pdf') }}">Registrations at faculties</a><br>
                 <a href="{{ asset('/files/OW_S2017/OW_S2017_CTU_Sports.pdf') }}">Sport at CTU</a><br>
                 <a href="{{ asset('/files/OW_S2017/OW_S2017_CTU_Visa.pdf') }}">Visa</a><br>
                 <a href="{{ asset('/files/OW_S2017/OW_S2017_Czech_courses.pdf') }}">Czech courses</a>
             </div>
-        
+
 
     </div>
 
