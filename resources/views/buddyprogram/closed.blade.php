de2d4d3c7f697e2b80905083aab902bb6f6e8ea5
Martin Průcha <prucma@gmail.com>
2017-03-18 19:11:23 +0100

    Change closed buddyprogram page

--- resources/views/buddyprogram/closed.blade.php
+++ resources/views/buddyprogram/closed.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.buddyprogram.layout')
 
 @section('content')
-    <h2>Databáze zahraničních studentů ještě nebyla otevřena. Spuštění Buddy Programu pro zimní semestr je plánováno na pátek 20.1.2017</h2>
+    <h2>Databáze zahraničních studentů ještě nebyla otevřena. Datum spuštění Buddy Programu pro letní semestr 2017 bude upřesněno</h2>
     <div class="col-md-12 hidden-xs">
         <!-- img src="{{ asset('img/buddyprogram/buddy_databaze_stat.png') }}" style="width:90%" -->
     </div>
