ec8cd6535ad6027958d465176f3ce900c8a48a36
David Mládek <mladedav@fit.cvut.cz>
2017-03-26 23:53:47 +0200

    no comment

--- app/Http/Controllers/Api/GithubController.php
+++ app/Http/Controllers/Api/GithubController.php
@@ -11,24 +11,21 @@ class GithubController extends Controller
 {
     public function githubUpdate(Request $request)
     {
+        shell_exec('echo "Hook caught" >> /var/www/deploy-scripts/log');
 
-	$payload = decodePayload($request->get('payload'));
-    //$payload = $request;
-	if ( $payload['ref'] != 'refs/heads/master' )
-	{
-		shell_exec('echo "Ignoring push to ' . $request->payload('ref') . '." >> /var/www/deploy-scripts/log');
-		return;
-	}
+        $payload = json_decode($request->get('payload'));
+        shell_exec('echo "Payload generated" >> /var/www/deploy-scripts/log');
+        //$payload = $request;
+        if ( $payload['ref'] != 'refs/heads/master' )
+        {
+            shell_exec('echo uncool >> /var/www/deploy-scripts/log');
+            shell_exec('echo "' . $request->get('payload') . '" >> /var/www/deploy-scripts/log' );
+            shell_exec('echo "Ignoring push to ' . $payload['ref'] . '." >> /var/www/deploy-scripts/log');
+            return;
+        }
+        print ('cool');
 	shell_exec('/var/www/deploy-scripts/iscweb');
     }
-
-
-    public function decodePayload($payload)
-    {
-        return json_decode($payload);
-    }
-
-
 }
 
 [
