034cfc2d25d1e185cd6289ca23c4d91516083602
fm-mf <filip.marek.95@gmail.com>
2017-06-06 20:10:24 +0200

    Update closed.blade.php

--- resources/views/buddyprogram/closed.blade.php
+++ resources/views/buddyprogram/closed.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.buddyprogram.layout')
 
 @section('content')
-    <h2>Databáze zahraničních studentů ještě nebyla otevřena. Datum spuštění Buddy Programu pro letní semestr 2017 bude upřesněno</h2>
+    <h2>Databáze zahraničních studentů ještě nebyla otevřena. Buddy Program pro zimní semestr 2017 bude spuštěn v září.</h2>
     <div class="col-md-12 hidden-xs">
-        <!-- img src="{{ asset('img/buddyprogram/buddy_databaze_stat.png') }}" style="width:90%" -->
+        <img src="{{ asset('img/buddyprogram/buddy_databaze_stat.png') }}" style="width:90%">
     </div>
 @stop
