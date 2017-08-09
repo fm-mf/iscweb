61d52e616c29d9fa75e892921fb4374f393cb507
Martin Prucha <prucma@gmail.com>
2017-08-09 20:12:52 +0200

    Registration email update

--- resources/views/emails/registrationsmail.blade.php
+++ resources/views/emails/registrationsmail.blade.php
@@ -4,27 +4,28 @@
     <title>ISC CTU in Prague Buddy Program</title>
 </head>
 <body>
-<p>Hi there!<br>It's less than a month to your arrivals and we're doing our best to set up the Buddy program
-    for you!<br>
+<p>Hi there!<br>
+    It's few weeks to your arrivals and we're doing our best to set up the buddy program for you! <br>
 We would very much appreciate your input now.<br>
-We are preparing activities and events for the upcoming semester and we would like to know what your expectations are.
-    We will do our best to adjust the program to your wishes and suggestions. Please tell us, it will help us a lot to
-    improve - LINK TYPE FORM</p>
-<p>Our goal is to find a buddy for every one of you. But since all buddies are volunteers, we can't promise anything.
+    We are preparing activities and events for the upcoming semester and we would like to know what your expectations are.
+    We will do our best to adjust the program to your wishes and suggestions. Please tell us by filling out this Expectation
+    form, it will help us a lot: <a href="https://iscctu.typeform.com/to/jR6TQo">https://iscctu.typeform.com/to/jR6TQo</a>
+
+<p>Also, our goal is to find a buddy for every one of you. But since all buddies are volunteers, we can't promise anything.
     What will help, however, is if you fill out the following form:</p>
 
 <p><a href="https://isc.cvut.cz/exchange/{{ $hash }}">https://isc.cvut.cz/exchange/{{ $hash }}</a></p>
 
 <p><b>Please, keep the link for future updates of your arrival information.</b></p>
-<p>The information you share will be visible to Czech buddies, so that it would be easier for them to choose based
-    on the <b>date of your arrival</b> and things you might have in common. Exchange students, who submit the form,
-    will be given <b>priority in the program</b>. So go ahead! You can fill information about your <b>hobbies</b>,
-    about your <b>field of study</b> and even about the reason of choosing Prague as your exchange destination. :)</p>
-<!-- nova data v emailu -->
+<p>The information you share will be visible to Czech buddies, so that it would be easier for them to choose based on the
+    <b>date of your arrival</b> and things you might have in common. Exchange students, who submit the form, will be given
+    <b>priority in the program</b>. So go ahead! You can fill information about your <b>hobbies</b>, about your
+    <b>field of study</b> and even about the
+    reason of choosing Prague as your exchange destination. :)
 <p>If, for any reason, you don't wish to have a Czech buddy, you can also take your name out of the program by checking
     the "I don't wish to have a buddy" option in the same form. Just remember that taking part in the buddy program is
     the best way to meet Czech people, learn something about us and our culture and, above all, it can make your first
-    days in Prague significantly easier, since not all people in Prague speak English.</p>
+    days in Prague significantly easier, since not all people in Prague speak <English class=""></English></p>
 
 <p>You can find more information about the International Student Club and our activities on our website
     <a href="https://www.isc.cvut.cz">www.isc.cvut.cz</a>.
@@ -33,6 +34,6 @@ We are preparing activities and events for the upcoming semester and we would li
 <p>We can't wait to meet you in Prague.<br>Volunteers of International Student Club CTU in Prague</p>
 
 <p>Web:<a href="https://www.isc.cvut.cz">www.isc.cvut.cz</a><br>FB page: <a href="https://www.facebook.com/isc.ctu.prague/">https://www.facebook.com/isc.ctu.prague/</a>
-    <br>FB group: <a href="https://www.facebook.com/groups/118676091960293/">https://www.facebook.com/groups/118676091960293/</a></p>
+    <br>FB group: <a href="https://www.facebook.com/groups/iscctufall2017">https://www.facebook.com/groups/iscctufall2017</a></p>
 </body>
 </html>
