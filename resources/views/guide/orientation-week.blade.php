8bc40949122aa7e5ff851899360e4c21ca3b7f00
Filip Marek <filip.marek.95@gmail.com>
2017-08-22 15:23:44 +0200

    Update Orientation week schedule in Survival Guide

--- resources/views/guide/orientation-week.blade.php
+++ resources/views/guide/orientation-week.blade.php
@@ -6,128 +6,221 @@
     </div>
 
     <div class="row">
-        <div class="col-md-12 bg-danger">It is strongly recommended to attend the Orientation Week, which is held in the last
-            week before classes start. During this week, international students are introduced to
-            the CTU educational system and are given information for their stay in the Czech Republic. They are helped to register for their courses, they meet Czech and international
-            students, make some trips, and generally settle in before the semester begins. Do not
-            miss this good opportunity.
+        <div class="col-md-12 bg-danger">
+            <p>Orientation week is held by ISC one week before semester starts and it provides you many important informations
+                which can simplify your first days of your stay in the Czech Republic.
+                It is strongly recommended to attend this program.
+                During this week you will get to know the main bureaucracy at the CTU.
+                Besides, we will help you to make new local and international friends through parties, games and a lot of trips.
+                You shouldn’t miss this great opportunity!
+            </p>
         </div>
-    </div>
-    <!--div class="row">
+    </div>{{--
+    <div class="row">
         <p>
             See <a href="https://isc.cvut.cz/ow">Meeting points and times</a>
         </p>
-    </div -->
+    </div>
 
-    <!--The schedule of the Orientation Week will be announced soon. Stay tuned!-->
+    The schedule of the Orientation Week will be announced soon. Stay tuned!--}}
 
     <div class="row">
-        <h3>{{ $owFromTo }}</h3>
+        <h2>{{ $owFromTo }}</h2>
     </div>
 
     <div class="row">
-        <h3>DAY 1 - Monday <b>(February 13th)</b></h3>
-    </div>
-    <div class="row">
-        <div class="col-lg-12">
-            Initial information meeting at <b>10:30</b> - 14:30, <b>Faculty of Civil Engineering, room D-1122</b>
-            <ul>
-                <li>Official welcome</li>
-                <li>How to "survive" in Prague</li>
-                <li>Visa regulations and insurance policy</li>
-                <li>Arranging all the necessary paperwork</li>
-                <li>ISC membership</li>
-                <li>Introduction to the Czech Culture Course</li>
-                <li>Life in the Czech Republic</li>
-                <li>How to enroll in courses</li>
-
-            </ul>
-        </div>
-        <div class="col-lg-12">
-            Tandem Evening (more info soon)
-            <ul>
-                <li>Enjoy the first beer of the semester and get involved in our unique language learning program! Here you can meet a tandem partner to practice your language skills and have a drink.</li>
-            </ul>
-        </div>
-        <h4>OW presentation Monday</h4>
-
-            <div class="col-lg-12">
-                <a href="{{ asset('/files/OW_S2017/OW_S2017_Meeting_points_Registrations_at_faculties.pdf') }}">Registrations at faculties</a><br>
-                <a href="{{ asset('/files/OW_S2017/OW_S2017_CTU_Sports.pdf') }}">Sport at CTU</a><br>
-                <a href="{{ asset('/files/OW_S2017/OW_S2017_CTU_Visa.pdf') }}">Visa</a><br>
-                <a href="{{ asset('/files/OW_S2017/OW_S2017_Czech_courses.pdf') }}">Czech courses</a><br>
-                <a href="{{ asset('/files/OW_S2017/KOS_manual_CTU_Spring_2017.pdf') }}">KOS manual</a>
-            </div>
-
-
-    </div>
-
-    <div class="row">
-        <h3>DAY 2 - Tuesday (February 14th)</h3>
+        <h2>DAY 1 - Monday <strong>(September 25th)</strong></h2>
     </div>
     <div class="row">
         <div class="col-lg-12">
-            In the morning - <strong>Registration</strong> at the faculties (for Erasmus and Exchange students).<br><b>You will need:</b>
-            <ul>
-                <li><b>Study record book</b> (green booklet) from the Welcome Pack</li>
-                <li>Passport photo</li>
-                <li>Pen</li>
+            <h3>Initial information meeting</h3>
+            <ul class="list-unstyled">
+                <li>When: <strong>10:30</strong> - 15:00</li>
+                <li>Where: <a href="https://goo.gl/maps/pHLFL6ZP8uP2" target="_blank"><strong>The Bethlehem Chapel</strong> (Betlémské náměstí 255/4, Praha 1)</a></li>
+                <li>Meeting points:
+                    <ul>
+                        <li>9:00 at Strahov</li>
+                        <li>9:30 in the lobby of the Masarykova dormitory</li>
+                    </ul>
+                </li>
+                <li>Topics:
+                    <ul>
+                        <li>Official welcome</li>
+                        <li>How to enroll in courses and sports at the CTU</li>
+                        <li>Czech language and Czech culture courses</li>
+                        <li>Life in the Czech Republic and how to survive in Prague</li>
+                        <li>ESN membership</li>
+                        <li>ISC teams and activities</li>
+                        <li>Insurance and Visa regulations</li>
+                    </ul>
+                </li>
             </ul>
         </div>
     </div>
     <div class="row">
         <div class="col-lg-12">
-            2nd information meeting at 14:00 - 16:30, Congress hall of the Masarykova dormitory
-            <ul>
-                <li>ISC trip registrations (ESN card & cash needed)</li>
+            <h3>Tandem Evening</h3>
+            <p>A unique program for improving your skills organized by ISC Languages team</p>
+            <ul class="list-unstyled">
+                <li>When: TBA</li>
+                <li>Where: TBA</li>
+                <li>Meeting points:
+                    <ul>
+                        <li>At Strahov</li>
+                        <li>In the lobby of the Masarykova dormitory</li>
+                    </ul>
+                </li>
+                <li>See: <a href="{{ url('activities/language-programs') }}" target="_blank">{{ url('activities/language-programs') }}</a></li>
             </ul>
         </div>
     </div>
+    {{--
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>OW presentation Monday</h3>
+            <ul class="list-unstyled">
+                <li><a href="{{ asset('/files/OW_S2017/OW_S2017_Meeting_points_Registrations_at_faculties.pdf') }}">Registrations at faculties</a></li>
+                <li><a href="{{ asset('/files/OW_S2017/OW_S2017_CTU_Sports.pdf') }}">Sport at CTU</a></li>
+                <li><a href="{{ asset('/files/OW_S2017/OW_S2017_CTU_Visa.pdf') }}">Visa</a></li>
+                <li><a href="{{ asset('/files/OW_S2017/OW_S2017_Czech_courses.pdf') }}">Czech courses</a></li>
+                <li><a href="{{ asset('/files/OW_S2017/KOS_manual_CTU_Spring_2017.pdf') }}">KOS manual</a></li>
+            </ul>
+        </div>
+    </div>
+    --}}
+
+    <div class="row">
+        <h2>DAY 2 - Tuesday (September 26th)</h2>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>Registation at faculties</h3>
+            <p class="bg-info">
+                Note: <strong>Only for Erasmus students and Exchange students</strong>. Self-paying and Erasmus Mundus students, please contact Faculty coordinators at your study department.
+            </p>
+            <ul class="list-unstyled">
+                <li>When: Various times for different faculties</li>
+                <li>Where: At your faculty (more informations in the presentations on the first day)</li>
+                <li class="bg-danger">You will need:
+                    <ul>
+                        <li>The green <strong>study record book</strong> from the Welcome pack</li>
+                        <li>A passport photo</li>
+                        <li>A pen</li>
+                    </ul>
+                </li>
+                <li>You will get:
+                    <ul>
+                        <li>The list of your classes</li>
+                        <li>Your schedule</li>
+                        <li>Your email account</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>Second information meeting</h3>
+            <ul class="list-unstyled">
+                <li>When: <strong>14:00</strong> - 16:30</li>
+                <li>Where: Congress Hall of the Masarykova dormitory</li>
+                <li>Topics:
+                    <ul>
+                        <li>ISC trips presentations and registrations (<strong>ESNcard + cash needed</strong>)</li>
+                        <li>ISC care</li>
+                        <li>Your email account</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>Blind Date</h3>
+            <p>An interesting dating game organized by ISC Care team</p>
+            <ul class="list-unstyled">
+                <li>When: in the evening</li>
+                <li>Where: TBA</li>
+            </ul>
+        </div>
+    </div>
+    {{--
     <div class="row">
         <div class="col-lg-12">
             <a href="{{ asset('/files/OW_S2017/OW_S2017_Meeting_points_Registrations_at_faculties.pdf') }}">Meeting points - Tuesday</a>
         </div>
     </div>
+    --}}
 
     <div class="row">
-        <h3>DAY 3 - Wednesday (February 15th)</h3>
+        <h2>DAY 3 - Wednesday (September 27th)</h2>
     </div>
     <div class="row">
         <div class="col-lg-12">
-            Prague Discovery Game (PDG) at 13:30, Faculty of Electrical Engineering, room 256
-            <ul>
-                <li>Great treasure hunt organized by ISC</li>
+            <h3>Registation at faculties</h3>
+            <p class="bg-info">Apply the same informations as for DAY 2</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>Prague Discovery Game (PDG)</h3>
+            <p>A great treasure hunt across Prague organized by ISC Activities team</p>
+            <ul class="list-unstyled">
+                <li>When: in the afternoon</li>
+                <li>Where: Faculty of Mechanical Engineering, room 256</li>
             </ul>
         </div>
     </div>
     <div class="row">
         <div class="col-lg-12">
-            inteGREATion party in the evening at Storm Club
-            <ul>
-                <li>Get to know each other at the first ISC inteGREAT party and have fun!</li>
+            <h3>inteGREATion party</h3>
+            <p>Get to know each other at the very first ISC inteGREAT party and have fun!</p>
+            <ul class="list-unstyled">
+                <li>When: 21:00</li>
+                <li>Where: <a href="https://goo.gl/maps/NYAwpVKPeyJ2" target="_blank">P.M. Club (Trojická 10, Praha 2)</a></li>
+                <li>Meeting points:
+                    <ul>
+                        <li>At Strahov</li>
+                        <li>In the lobby of the Masarykova dormitory</li>
+                    </ul>
+                </li>
             </ul>
         </div>
     </div>
 
     <div class="row">
-        <h3>DAY 4-7 - Thursday to Sunday (February 16th – 19th) </h3>
+        <h2>DAY 4-7 - Thursday to Sunday (September 28th – October 1st) </h2>
     </div>
     <div class="row">
         <div class="col-lg-12">
-            Trips around the Czech Republic
-            <ul>
-                <li>Various trips (both one-day and weekend events) to many interesting places in the Czech Republic.</li>
-                <li>A great opportunity to get to know other students!</li>
+            <h3>Registation at faculties</h3>
+            <p class="bg-info">Applies the same informations as for DAY 2</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>Trips around the Czech Republic</h3>
+            <p>One-day or two-day events introducing you to a lot of interesting places</p>
+            <ul class="list-unstyled">
+                <li>When/Where: various times and places for each trip, more informations on DAY 2 at the second information meeting</li>
+                <li class="bg-danger">You will need:
+                    <ul>
+                        <li>Cash</li>
+                        <li>Little snacks and beverage</li>
+                        <li>Comfortable clothes and shoes</li>
+                    </ul>
+                </li>
             </ul>
         </div>
     </div>
-    <br>
 
     <div class="row">
-        <div class="col-md-12 bg-danger">Please keep in mind there are four types of students: Erasmus students,
-            Exchange students, Erasmus Mundus students and Private (self-paying)
-            students.  Private  (self-paying)  students  and  Erasmus  Mundus  students
-            please contact their coordinators at their study department for information about visa issues, registration at faculties, etc.
+        <div class="col-md-12 bg-danger">
+            <p>Please keep in mind there are four types of students: Erasmus students,
+                Exchange students, Erasmus Mundus students and Private (self-paying)
+                students.  Private  (self-paying)  students  and  Erasmus  Mundus  students
+                please contact their coordinators at their study department for information about visa issues, registration at faculties, etc.
+            </p>
         </div>
     </div>
 @stop
