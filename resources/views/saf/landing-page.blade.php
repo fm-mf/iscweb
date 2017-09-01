e2bce38bc0aa681b67d0d33a6189e7e9d0ec4844
Tucnace <tucnace@gmail.com>
2017-09-01 23:59:46 +0200

    SAF web

--- resources/views/saf/landing-page.blade.php
+++ resources/views/saf/landing-page.blade.php
@@ -1,4 +1,7 @@
-
-<div id="landing-page" class="fullscreen">
-    <h1>S &Ccaron;VUT do sv&ecaron;ta</h1>
+<div class="fullscreen">
+    <div id="landing-page">
+        <img id="saf-logo" src="/img/saf/isc-saf-logo.png"/>
+        <h1>S &Ccaron;VUT do sv&ecaron;ta</h1>
+        <span class="more"><span class="double-arrow-down">M</span>Zjisit v&iacute;ce<span class="double-arrow-down">M</span></span>
+    </div>
 </div>
