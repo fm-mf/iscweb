453d1a9057d0a87250d63839eda5b4f3cf2d6c34
zemanm30 <zemanm30@fit.cvut.cz>
2018-05-18 22:15:00 +0200

    Added Privacy pages

---
+++ resources/views/web/privacy/tree.blade.php
@@ -0,0 +1,26 @@
+{{--/**
+ * Created by PhpStorm.
+ * User: mobil
+ * Date: 18/05/2018
+ * Time: 20:51
+ */--}}
+@extends('web.layouts.subpage')
+@section('wrapper-class', 'about-wrapper')
+@section('navClass', 'navbar-dark')
+@section('title', 'Privacy')
+@section('titleClass','title-privacy')
+
+@section('content')
+
+    <div class="container subpage">
+        <div class="row vision">
+            <div class="col-sm-6">
+                <p><a href="{{ url('privacy/notice') }}">Privacy notice</a></p>
+                <p><a href="{{ url('privacy/policy') }}">Privacy policy</a></p>
+            </div>
+            <div class="col-sm-6">
+                <p><a href="{{ url('privacy/agreements-cs') }}">Souhlas se zpracováním osobních údajů</a></p>
+            </div>
+        </div>
+    </div>
+@endsection
