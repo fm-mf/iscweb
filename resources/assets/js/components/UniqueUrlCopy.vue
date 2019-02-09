2c05886a676e95d605f0100f3241bd2ce5e33b11
Martin Prucha <prucma@gmail.com>
2019-02-09 18:04:08 +0100

    Update uniqueUrlCopy component.
    Update fix behaviour of the tooltip.

--- resources/assets/js/components/UniqueUrlCopy.vue
+++ resources/assets/js/components/UniqueUrlCopy.vue
@@ -3,11 +3,47 @@
         <span class="input-group-addon" id="url"><span class="glyphicon glyphicon-link"></span> </span>
         <input type="text" id="unique_url" class="form-control" :value="url" aria-describedby="url" readonly="">
         <span class="input-group-btn">
-            <button class="btn btn-default" type="button" @click="copy" data-toggle="tooltip" data-placement="right" v-bind:title="buttonTitle"><span class="glyphicon glyphicon-copy"></span> </button>
+            <button
+                class="btn btn-default"
+                type="button"
+                @mouseleave="mouseLeave"
+                @click="copy"
+                v-tooltip:right="tooltipTest"
+            >
+                <span class="glyphicon glyphicon-copy"></span>
+            </button>
         </span>
     </div><!-- /input-group -->
 </template>
 <script>
+    function bsTooltip(el, binding, updating = false) { 
+        let trigger;
+        if (binding.modifiers.focus || binding.modifiers.hover || binding.modifiers.click) {
+            const t = [];
+            if (binding.modifiers.focus) t.push('focus');
+            if (binding.modifiers.hover) t.push('hover');
+            if (binding.modifiers.click) t.push('click');
+            trigger = t.join(' ');
+        }
+		console.log('TCL: bsTooltip -> binding', binding)
+        $(el).tooltip({
+            title: binding.value,
+            placement: binding.arg,
+            trigger: trigger,
+            html: binding.modifiers.html
+        });
+        // To update title after click
+        if (updating) {
+            $(el)
+                .attr('title', binding.value)
+                .tooltip('fixTitle')
+            // HACK: Not show when mouse is left.
+            if (binding.value !== 'Copy')
+                $(el).tooltip('show')
+        }
+    }
+
+
     export default {
 
         props: {
@@ -19,12 +55,27 @@
 
         data () {
             return {
-                buttonTitle: 'fkdfjdkf',
+                buttonTitle: 'Copy',
                 show: false,
             }
         },
 
         computed: {
+            tooltipTest: function() {
+                return this.buttonTitle;
+            }
+        },
+
+        directives: {
+            tooltip: {
+                bind: bsTooltip,
+                update (el, binding) {
+                    bsTooltip(el, binding, true)
+                },
+                unbind (el, binding) {
+                    $(el).tooltip('destroy');
+                }
+            }
         },
 
         methods: {
@@ -34,15 +85,10 @@
                 var suc = document.execCommand('copy');
                 this.buttonTitle = 'Copied!';
                 //show = true;
-
             },
-
-        },
-        mounted() {
-            this.$nextTick(() => {
-                $('[data-toggle="tooltip"]').tooltip()
-            });
+            mouseLeave() {
+                this.buttonTitle = 'Copy'
+            }
         }
-
     };
 </script>
