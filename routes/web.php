70a2ee7d81bf5812046a216d11c13638f5b1898e
Petr Fiedler <tucnace@gmail.com>
2019-03-14 18:43:08 +0100

    Redirect to NAS working only when in Point

--- routes/web.php
+++ routes/web.php
@@ -53,8 +53,9 @@ Route::group(['namespace' => 'Web', 'prefix' => ''], function()
     Route::get('/contact', 'WebController@showContacts');
     Route::get('/calendar', 'WebController@showCalendar');
     Route::get('/buddy', function () { return view('web.buddy'); });
-    Route::get('/nas', function () { return redirect('https://147.32.97.62:5001'); })->name('nas');
-
+    //Route::get('/nas', function () { return redirect('https://147.32.97.62:5001'); })->name('nas');
+    Route::get('/nas', function () { return redirect('https://192.168.0.102:5001'); })->name('nas');
+    
     Route::post('/voting/process', 'VotingController@processVoting');
     Route::get('/voting/results', 'VotingController@showResults')->middleware(['checkpartak', 'auth']);
     Route::get('/voting/thank-you', 'VotingController@showThankYou');
