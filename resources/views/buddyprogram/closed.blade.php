7a0747018474b961708545468563ed07919688b1
Jan Páleník <johnatko@gmail.com>
2019-07-11 17:50:56 +0200

    pridan bootstrap na buddy databazi, uprava vzhledu a rozvrzeni webu

--- resources/views/buddyprogram/closed.blade.php
+++ resources/views/buddyprogram/closed.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.buddyprogram.layout')
 
 @section('content')
-    <h2>Databáze zahraničních studentů ještě nebyla otevřena. Buddy Program pro zimní semestr 2018/2019 bude spuštěn 1. září 2018.</h2>
-    <p>Pro více informací sleduj naší facebookovou skupinu <a href="https://www.facebook.com/groups/isc.ctu.buddies" target="_blank">ISC CTU Czech buddies</a>
-     a také naší facebookovou stránku <a href="https://www.facebook.com/isc.ctu.prague" target="_blank">ISC CTU in Prague</a>.</p>
-    <div class="col-md-12 hidden-xs">
-        <img src="{{ asset('img/buddyprogram/buddy-databaze-mapa-fall2018.jpg') }}" style="width:90%">
+    <div class="container page">
+        <h2 class="text-center">Databáze zahraničních studentů ještě nebyla otevřena. Buddy Program pro zimní semestr 2019/2020 bude spuštěn 1. září 2019.</h2>
+        <p class="text-center">Pro více informací sleduj naší facebookovou skupinu <a href="https://www.facebook.com/groups/isc.ctu.buddies" target="_blank">ISC CTU Czech buddies</a>
+         a také naší facebookovou stránku <a href="https://www.facebook.com/isc.ctu.prague" target="_blank">ISC CTU in Prague</a>.</p>
+        <div class="col-md-12 hidden-xs">
+            <img src="{{ asset('img/buddyprogram/buddy-databaze-mapa-fall2018.jpg') }}" style="width: 100%">
+        </div>
     </div>
 @stop
--- resources/views/buddyprogram/profile.blade.php
+++ resources/views/buddyprogram/profile.blade.php
@@ -2,56 +2,59 @@
 
 @section('content')
 
-    <div class="container-fluid page">
-        <h1>Profil zahraničního studenta</h1>
+    <div class="container page">
+        <div class="card-body">
+            <h1 class="text-center card-title">Profil zahraničního studenta</h1>
 
-        <div class="row">
-            <div class="col-sm-4 col-lg-2">
-                @if ($avatar)
-                <div class="avatar-view" title="Profile picture">
-                    <img src="{{ asset($avatar) }}" alt="Avatar" id="avatar">
+            <div class="row">
+                <div class="col-sm-8 col-lg-9">
+                    <h2>{{ $exchangeStudent->person->first_name }} {{ $exchangeStudent->person->last_name }}</h2>
+                    <div class="row">
+                        <div class="col">
+                            @if($exchangeStudent->country) <strong>Země</strong>: {{ $exchangeStudent->country->full_name }}<br> @endif
+                            @if($exchangeStudent->school != "NULL") <strong>Škola</strong>: {{ $exchangeStudent->school }}<br> @endif
+                            @if($exchangeStudent->person->sex) <strong>Pohlaví</strong>: {{ $exchangeStudent->person->sex === "F" ? "žena" : "muž" }}<br> @endif
+                            @if($exchangeStudent->person->age)<strong>Věk</strong>: {{ date("Y") - $exchangeStudent->person->age -1}}-{{date("Y") - $exchangeStudent->person->age }} let <br>@endif
+                            @if($exchangeStudent->faculty)<strong>Fakulta ČVUT</strong>: {{ $exchangeStudent->faculty->faculty }}<br> @endif
+                            <strong>Samoplátce</strong>: {{ $exchangeStudent->isSelfPaying() ? 'ano' : 'ne' }}<br>
+                            <strong>Datum příjezdu</strong>:
+                            @if($exchangeStudent->arrival)
+                                {{ $exchangeStudent->arrival->arrival->format('j. n. Y H:i') }} <br>
+                            @else
+                                Zatím nevyplněno <br>
+                            @endif
+                            <strong>Způsob příjezdu</strong>:
+                            @if($exchangeStudent->arrival)
+                                {{ $exchangeStudent->arrival->transportation->transportation }} <br>
+                            @else
+                                Zatím nevyplněno <br>
+                            @endif
+                            <strong>Ubytování</strong>: {{ $exchangeStudent->accommodation->full_name }} <br>
+                        </div>
+                    </div>
                 </div>
-                @endif
+                <div class="col-sm-4 col-lg-3">
+                    @if ($avatar)
+                        <div class="avatar-view" title="Profile picture">
+                            <i class="fas fa-user-circle" id="avatar"></i>
+                        <!--<img src="{{ asset($avatar) }}" alt="Avatar" id="avatar">-->
+                        </div>
+                    @endif
+                </div>
             </div>
-            <div class="col-sm-8 col-lg-10">
-                <h2>{{ $exchangeStudent->person->first_name }} {{ $exchangeStudent->person->last_name }}</h2>
+        </div>
+        <div class="card-body">
+            @if ($exchangeStudent->about)
+
                 <div class="row">
-                    <div class="col-sm-6">
-                        @if($exchangeStudent->country) <strong>Země</strong>: {{ $exchangeStudent->country->full_name }}<br> @endif
-                        @if($exchangeStudent->school != "NULL") <strong>Škola</strong>: {{ $exchangeStudent->school }}<br> @endif
-                        @if($exchangeStudent->person->sex) <strong>Pohlaví</strong>: {{ $exchangeStudent->person->sex === "F" ? "žena" : "muž" }}<br> @endif
-                        @if($exchangeStudent->person->age)<strong>Věk</strong>: {{ date("Y") - $exchangeStudent->person->age -1}}-{{date("Y") - $exchangeStudent->person->age }} let <br>@endif
-                        @if($exchangeStudent->faculty)<strong>Fakulta ČVUT</strong>: {{ $exchangeStudent->faculty->faculty }}<br> @endif
-                        <strong>Samoplátce</strong>: {{ $exchangeStudent->isSelfPaying() ? 'ano' : 'ne' }}<br>
-                    </div>
-                    <div class="col-sm-6">
-                        <strong>Datum příjezdu</strong>:
-                        @if($exchangeStudent->arrival)
-                            {{ $exchangeStudent->arrival->arrival->format('j. n. Y H:i') }} <br>
-                        @else
-                            Zatím nevyplněno <br>
-                        @endif
-                        <strong>Způsob příjezdu</strong>:
-                        @if($exchangeStudent->arrival)
-                            {{ $exchangeStudent->arrival->transportation->transportation }} <br>
-                        @else
-                            Zatím nevyplněno <br>
-                        @endif
-                        <strong>Ubytování</strong>: {{ $exchangeStudent->accommodation->full_name }} <br>
+                    <div class="container">
+                        <h3>Podrobosti</h3>
+                        <p>{{ $exchangeStudent->about }}</p>
                     </div>
                 </div>
-            </div>
+            @endif
         </div>
-        @if ($exchangeStudent->about)
-
-        <div class="row">
-            <div class="col-xs-12">
-                <h3>Podrobosti</h3>
-                <p>{{ $exchangeStudent->about }}</p>
-            </div>
-        </div>
-        @endif
-        <div class="row buddy-actions">
+        <div class="card-body">
             @if(count($errors) > 0)
                 @foreach($errors->all() as $error)
                     <div class="flash col-sm-6">{{$error}}</div>
@@ -60,17 +63,16 @@
             @if(!$exchangeStudent->hasBuddy() && $casChoose)
                 <a href="{{ url('muj-buddy/become-buddy/' . $exchangeStudent->id_user) }}" class="btn btn-primary btn">Staň se jeho / jejím Buddym.</a>
             @elseif ($exchangeStudent->id_buddy === Auth::id())
-                <div class="show-email col-sm-6">
-                    <strong>{{ $exchangeStudent->person->first_name }} {{ $exchangeStudent->person->last_name }}</strong> je tvým Buddym!
+                <div class="show-email">
+                    <strong>{{ $exchangeStudent->person->first_name }} {{ $exchangeStudent->person->last_name }}</strong> je tvým Buddym! <br>
                     Teď mu můžeš napsat na <a href="mailto:{{ $exchangeStudent->person->user->email }}">{{ $exchangeStudent->person->user->email }}</a>
                 </div>
             @else
-                    <div class="show-email col-sm-6">
-                        <p>Vyčerpán denní limit (1) pro výběr zahraničních studentů</p>
-                    </div>
+                <div class="show-email">
+                    <p>Vyčerpán denní limit (1) pro výběr zahraničních studentů</p>
+                </div>
             @endif
-            <p class="show-info col-sm-12">V případě, že chceš zahraničního studenta odebrat ze svých buddíků, napiš na <a href="mailto:buddy@isc.cvut.cz">buddy@isc.cvut.cz</a></p>
-
+            <p class="show-info">V případě, že chceš zahraničního studenta odebrat ze svých buddíků, napiš na <a href="mailto:buddy@isc.cvut.cz">buddy@isc.cvut.cz</a></p>
         </div>
     </div>
 
