11c6a1e0aac7d5e53dd79cf1b6d737139d05508b
hajekjiri <jiri.hajek18@gmail.com>
2019-07-11 20:10:08 +0200

    Update hardcoded buddy program start date

--- resources/views/buddyprogram/closed.blade.php
+++ resources/views/buddyprogram/closed.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.buddyprogram.layout')
 
 @section('content')
-    <h2>Databáze zahraničních studentů ještě nebyla otevřena. Buddy Program pro zimní semestr 2018/2019 bude spuštěn 1. září 2018.</h2>
+    <h2>Databáze zahraničních studentů ještě nebyla otevřena. Buddy Program pro zimní semestr 2019/2020 bude spuštěn 30. srpna 2019.</h2>
     <p>Pro více informací sleduj naší facebookovou skupinu <a href="https://www.facebook.com/groups/isc.ctu.buddies" target="_blank">ISC CTU Czech buddies</a>
      a také naší facebookovou stránku <a href="https://www.facebook.com/isc.ctu.prague" target="_blank">ISC CTU in Prague</a>.</p>
     <div class="col-md-12 hidden-xs">
