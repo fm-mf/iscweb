2b209505d63b39c4f2326e92c7b9eda11554b5da
Filip Marek <filip.marek.95@gmail.com>
2019-09-10 15:44:45 +0200

    Update ESN and ČVUT logo in Survival Guide

--- resources/views/guide/esn-intro.blade.php
+++ resources/views/guide/esn-intro.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'ESN &ndash; ')
 
 @section('content')
+  <section id="esn-intro">
     <h1>Erasmus Student Network</h1>
       <p>Erasmus Student Network (ESN) is the biggest student association in Europe.
         It was born on the 16th October 1989 and legally registered in 1990
@@ -12,8 +13,9 @@
         We have around 15,000 active members that are in many sections (ISC CTU in Prague is one of them)
         supported by so called buddies taking care of international students.
         Thus, ESN involves around 40,000 young people offering its services to around 220,000 international students every year.</p>
+  </section>
 
-
+  <section id="esn-cz-intro">
       <h2>Erasmus Student Network Czech Republic</h2>
         <p>Erasmus Student Network Czech Republic (ESN CZ) is a non-for-profit student organisation representing
           local ESN sections on the national level.
@@ -21,5 +23,6 @@
           All together sections of ESN CZ (18 sections in total) help roughly 10 000 international students every academic year.
           ESN CZ represents international students, thus provide opportunities for cultural understanding and self-development.</p>
           <p><strong>Since 2003, International Student Club CTU in Prague is a proud member of ESN Czech Republic.</strong></p>
-          <img src="{{ asset('img/saf/logo-esn.png') }}" alt="Logo ESN" title="Logo ESN" />
+          <img src="{{ asset('img/logos/esn-cz/esn-cz-logo-colour.svg') }}" alt="Logo ESN" title="Logo ESN" />
+  </section>
 @stop
--- resources/views/layouts/guide/footer.blade.php
+++ resources/views/layouts/guide/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="row footer">
     <ul class="logos list-inline">
-        <li><a href="https://www.isc.cvut.cz"><img src="{{ asset('img/logos/isc-logo-white-horizontal.svg') }}" alt="ISC CTU in Prague"></a></li>
-        <li><a href="https://www.cvut.cz"><img src="{{ asset('img/guide/symbol_cvut_konturova_verze_negativ.svg') }}" alt="CTU in Prague"></a></li>
+        <li><a href="{{ route('web.index') }}"><img src="{{ asset('img/logos/isc-logo-white-horizontal.svg') }}" alt="ISC CTU in Prague"></a></li>
+        <li><a href="https://www.cvut.cz"><img src="{{ asset('img/logos/cvut/logo_CVUT_negativ.svg') }}" alt="CTU in Prague"></a></li>
     </ul>
     <p>&copy; {{ $year }} | {{ $officialName }}</p>
 
