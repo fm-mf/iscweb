ce757f8e430a9a44996976e1a8a61a2a8dd193bb
Filip Marek <filip.marek.95@gmail.com>
2019-09-14 18:53:32 +0200

    Update contacts

--- app/Helpers/Contacts.php
+++ app/Helpers/Contacts.php
@@ -12,7 +12,8 @@ class Contacts
     {
         $maleSilhouette = 'img/web/contacts/male-silhouette-150.jpg';
         $femaleSilhouette = 'img/web/contacts/female-silhouette-150.jpg';
-        $contactImageFolder = 'img/web/contacts/2019-spring';
+        $contactImageFolderOld = 'img/web/contacts/2019-spring';
+        $contactImageFolder = 'img/web/contacts/2019-fall';
 
         $this->contacts = collect([
             [
@@ -20,21 +21,21 @@ class Contacts
                 'position' => 'President',
                 'email' => 'president@isc.cvut.cz',
                 'phone' => '',
-                'avatar' => asset("{$contactImageFolder}/kuba-president.jpg")
+                'avatar' => asset("{$contactImageFolder}/president-KubaN.jpg")
             ],
             [
                 'name' => 'Michal Nečas',
                 'position' => 'Vicepresident',
                 'email' => 'vicepresident@isc.cvut.cz',
                 'phone' => '',
-                'avatar' => asset("{$contactImageFolder}/neci-vice.jpg")
+                'avatar' => asset("{$contactImageFolder}/vice-MichalN.jpg")
             ],
             [
                 'name' => 'David Mládek',
                 'position' => 'Treasurer',
                 'email' => 'treasurer@isc.cvut.cz',
                 'phone' => '+420 721 155 737',
-                'avatar' => asset("{$contactImageFolder}/david-treasurer.jpg")
+                'avatar' => asset("{$contactImageFolder}/treasurer-David.jpg")
             ],
             [
                 'name' => 'Eva Eöllősová',
@@ -48,35 +49,35 @@ class Contacts
                 'position' => 'Local Representative',
                 'email' => 'lr@isc.cvut.cz',
                 'phone' => '',
-                'avatar' => asset($maleSilhouette)
+                'avatar' => asset("{$contactImageFolder}/lr-KubaD.jpg")
             ],
             [
                 'name' => 'Michal Cihlář',
                 'position' => 'Human Resources',
                 'email' => 'hr@isc.cvut.cz',
                 'phone' => '',
-                'avatar' => asset($maleSilhouette)
+                'avatar' => asset("{$contactImageFolder}/hr-MichalC.jpg")
             ],
             [
                 'name' => 'Jan Vůjtěch',
                 'position' => 'Quality and Knowledge Manager',
                 'email' => 'knowledge@isc.cvut.cz',
                 'phone' => '+420 728 559 713',
-                'avatar' => asset("{$contactImageFolder}/honza-qak.jpg")
+                'avatar' => asset("{$contactImageFolderOld}/honza-qak.jpg")
             ],
             [
                 'name' => 'Victoria Usan',
                 'position' => 'Activities Coordinator',
                 'email' => 'activities@isc.cvut.cz',
                 'phone' => '',
-                'avatar' => asset($femaleSilhouette)
+                'avatar' => asset("{$contactImageFolder}/activities-Viky.jpg")
             ],
             [
                 'name' => 'Jakub Trhlík',
                 'position' => 'inteGREAT Coordinator',
                 'email' => 'integreat@isc.cvut.cz',
                 'phone' => '',
-                'avatar' => asset($maleSilhouette)
+                'avatar' => asset("{$contactImageFolder}/inteGREAT-KubaT.jpg")
             ],
             [
                 'name' => 'Jan Páleník',
@@ -90,48 +91,48 @@ class Contacts
                 'position' => 'Point Coordinator',
                 'email' => 'point@isc.cvut.cz',
                 'phone' => '+420 732 227 056',
-                'avatar' => asset('img/web/contacts/2017fall/it-filip.jpg'),
+                'avatar' => asset("{$contactImageFolder}/point-Filip.jpg")
             ],[
                 'name' => 'Rajmund Hruška',
                 'position' => 'Languages Coordinator',
                 'email' => 'languages@isc.cvut.cz',
                 'phone' => '+421 902 833 731',
-                'avatar' => asset("{$contactImageFolder}/rajmund-languages.jpg")
+                'avatar' => asset("{$contactImageFolderOld}/rajmund-languages.jpg")
             ],
             [
                 'name' => 'Jan Zípek',
                 'position' => 'Buddy Coordinator',
                 'email' => 'buddy@isc.cvut.cz',
                 'phone' => '',
-                'avatar' => asset($maleSilhouette)
+                'avatar' => asset("{$contactImageFolder}/buddy-Kamen.jpg")
             ],
             [
                 'name' => 'Dominik Bureš',
                 'position' => 'Alumni Coordinator',
                 'email' => 'alumni@isc.cvut.cz',
                 'phone' => '',
-                'avatar' => asset("{$contactImageFolder}/dominik-alumni.jpg")
+                'avatar' => asset("{$contactImageFolder}/alumni-Dominik.jpg")
             ],
             [
                 'name' => 'Antonín Ženíšek',
                 'position' => 'Sports Coordinator',
                 'email' => 'sports@isc.cvut.cz',
                 'phone' => '+420 725 483 392',
-                'avatar' => asset("{$contactImageFolder}/tonda-sports.jpg")
+                'avatar' => asset("{$contactImageFolderOld}/tonda-sports.jpg")
             ],
             [
                 'name' => 'Kristýna Švandová',
                 'position' => 'ISC Care Coordinator',
                 'email' => 'care@isc.cvut.cz',
                 'phone' => '+420 774 004 806',
-                'avatar' => asset("{$contactImageFolder}/tyna-care.jpg")
+                'avatar' => asset("{$contactImageFolder}/care-Tyna.jpg")
             ],
             [
                 'name' => 'Jan Batysta',
                 'position' => 'Visa Coordinator',
                 'email' => 'visa@isc.cvut.cz',
                 'phone' => '+420 777 048 852',
-                'avatar' => asset("{$contactImageFolder}/bautysta-visa.jpg")
+                'avatar' => asset("{$contactImageFolder}/visa-Baty.jpg")
             ],
             [
                 'name' => 'Martin Vlk',
