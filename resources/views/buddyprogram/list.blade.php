3af205be06e0d84e8cb426b5964c0d2915346c56
Jan Zipek <jan@zipek.cz>
2019-09-14 22:39:34 +0200

    Initial draft of more responsible buddies table

--- resources/views/buddyprogram/list.blade.php
+++ resources/views/buddyprogram/list.blade.php
@@ -34,7 +34,44 @@
                     <div v-show="loading" class="table-loader">
                         <i class="fas fa-circle-notch fa-spin"></i>
                     </div>
-                    <div class="table-responsive">
+
+                    <div class="list-table">
+                        <div class="div-tr div-header">
+                            <div class="div-cell name">
+                                <orderable-column v-model="sortBy" v-on:input="filterChanged" field="name">Jméno</orderable-column>
+                            </div>
+                            <div class="div-cell country">
+                                <orderable-column v-model="sortBy" v-on:input="filterChanged" field="country">Země</orderable-column>
+                            </div>
+                            <div class="div-cell school">
+                                <orderable-column v-model="sortBy" v-on:input="filterChanged" field="school">Škola</orderable-column>
+                            </div>
+                            <div class="div-cell faculty">
+                                <orderable-column v-model="sortBy" v-on:input="filterChanged" field="faculty">Fakulta</orderable-column>
+                            </div>
+                            <div class="div-cell arrival">
+                                <orderable-column v-model="sortBy" v-on:input="filterChanged" field="arrival">Příjezd</orderable-column>
+                            </div>
+                            <div class="div-cell accomodation">
+                                <orderable-column v-model="sortBy" v-on:input="filterChanged" field="accomodation">Bydlení</orderable-column>
+                            </div>
+                        </div>
+                        <div class="div-body">
+                            <div class="div-tr" v-for="student in data">
+                                <div class="div-cell name"><a href="{{url('/muj-buddy/profile/')}}" v-bind:href="'{{url('/muj-buddy/profile')}}/' + student.id_user">@{{ student.person.first_name }} <span class="last-name">@{{ student.person.last_name }}</span></a></div>
+                                <div class="div-cell country">@{{ student.country.full_name }}</div>
+                                <div class="div-cell school">@{{ student.school }}</div>
+                                <div class="div-cell faculty">@{{ student.faculty.abbreviation }}</div>
+                                <div class="div-cell arrival"><span v-if="student.arrival">@{{ student.arrival['arrivalFormatted'] }}</span></div>
+                                <div class="div-cell accomodation">@{{ student.accommodation.full_name }}</div>
+                            </div>
+                            <div class="div-tr table-empty" v-if="!loading && data.length === 0">
+                                <div class="div-cell">Nenalezen žádný student</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--<div class="table-responsive">
                         <table class="table table-hover table-striped">
                             <thead>
                             <tr>
@@ -60,7 +97,7 @@
                             </tr>
                             </tbody>
                         </table>
-                    </div>
+                    </div>-->
                     </div>
 
                     <nav aria-label="Page navigation" v-if="data && data.length > 0 && pagesCount > 1">
