1089d6cd3581db9e3b8b039e47e5acb6d6c8b905
Jan Zipek <jan@zipek.cz>
2019-12-11 22:34:49 +0100

    Updated buddy database opening time

--- resources/views/buddyprogram/closed.blade.php
+++ resources/views/buddyprogram/closed.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container page">
-        <h2 class="text-center">Databáze zahraničních studentů ještě nebyla otevřena. Buddy Program pro zimní semestr 2019/2020 bude spuštěn 30. srpna 2019.</h2>
+        <h2 class="text-center">Databáze zahraničních studentů ještě nebyla otevřena. Buddy Program pro letní semestr 2019/2020 bude spuštěn 24. ledna 2019.</h2>
         <p class="text-center">Pro více informací sleduj naší facebookovou skupinu <a href="https://www.facebook.com/groups/isc.ctu.buddies" target="_blank">ISC CTU Czech buddies</a>
          a také naší facebookovou stránku <a href="https://www.facebook.com/isc.ctu.prague" target="_blank">ISC CTU in Prague</a>.</p>
         <div class="col-md-12 hidden-xs">
