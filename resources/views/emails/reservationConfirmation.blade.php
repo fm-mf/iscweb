6336fbca898c7774e887f6442017ad695a479fb9
Jan Zipek <jan@zipek.cz>
2019-12-27 15:44:20 +0100

    Different email text for ow trips

--- resources/views/emails/reservationConfirmation.blade.php
+++ resources/views/emails/reservationConfirmation.blade.php
@@ -6,14 +6,22 @@
     </head>
     <body>
         <p>Hi,</p>
-        <p>
-            You've successfully reserved your spot for {{ $event->name }}.
-            Now you have until <strong>{{ $response->expirationDate() }}</strong> to go to <a href="https://isc.cvut.cz/contact" target="_blank">ISC Point</a> and pay to confirm your spot.
-            If you don't confirm your spot until {{ $response->expirationDate() }}, your reservation will be cancelled.
-        </p>
-        <p>If you can't participate and you didn't already confirm your reservation, please use following link to cancel it:</p>
-        <p><strong><a href="{{ url("/event/{$event->reservations_hash}/cancel/{$response->hash}") }}">Cancel reservation</a></strong></p>
-        <p>If you've already confirmed your reservation, contact organizers or ISC to let us know you're not coming.</p>
+        @if ($event->ow)
+            <p>
+                You've successfully reserved your spot for {{ $event->name }}. Now go to trip stand and confirm your spot
+                by paying the trip price. If you don't do this, your reservation will be cancelled!
+            </p>
+        @else
+            <p>
+                You've successfully reserved your spot for {{ $event->name }}.
+                Now you have until <strong>{{ $response->expirationDate() }}</strong> to go to <a href="https://isc.cvut.cz/contact" target="_blank">ISC Point</a> and pay to confirm your spot.
+                If you don't confirm your spot until {{ $response->expirationDate() }}, your reservation will be cancelled.
+            </p>
+            <p>If you can't participate and you didn't already confirm your reservation, please use following link to cancel it:</p>
+            <p><strong><a href="{{ url("/event/{$event->reservations_hash}/cancel/{$response->hash}") }}">Cancel reservation</a></strong></p>
+            <p>If you've already confirmed your reservation, contact organizers or ISC to let us know you're not coming.</p>
+        @endif
+
         <p>Details about the trip:</p>
         <p>
             <strong>When:</strong> {{ $event->trip->eventDateInterval(false) }}<br />
