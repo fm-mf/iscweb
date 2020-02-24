9cac1db58a7746d99dcb26a3d9c244766e3338fe
Jan Zípek <jan@zipek.cz>
2020-02-24 20:57:51 +0100

    Fixed contacts editor

--- resources/views/partak/settings/contacts/index.blade.php
+++ resources/views/partak/settings/contacts/index.blade.php
@@ -1,22 +1,19 @@
 @extends('partak.settings.layout')
 
 @section('inner-content')
-    <div class="row row-inner">
-        <div class="col-md-11 col-lg-10">
-            <div id="alert-success-wrapper">
-                @if(session('success'))
-                    <div class="alert alert-success alert-dismissable fade in">
-                        <button class="close" data-dismiss="alert" aria-label="close"><span class="fas fa-times"></span></button>
-                        <span class="fas fa-check"></span> @lang(session('success')['event'], ['position' => session('success')['position']])
-                    </div>
-                @endif
-            </div>
-            <div class="row">
-                <div class="col-xs-12">
-                    <a class="btn btn-success pull-right" href="{{ route('partak.settings.contacts.create') }}"><span class="fas fa-user-plus"></span> Add new contact</a>
-                </div>
-            </div>
-            <contacts-order></contacts-order>
+    @if(session('success'))
+        <div class="alert alert-success alert-dismissable fade in">
+            <button class="close" data-dismiss="alert" aria-label="close"><span class="fas fa-times"></span></button>
+            <span class="fas fa-check"></span> @lang(session('success')['event'], ['position' => session('success')['position']])
         </div>
+    @endif
+
+    <div class="container">
+        <div class="text-right">
+            <a class="btn btn-success" href="{{ route('partak.settings.contacts.create') }}">
+                <i class="fas fa-user-plus"></i> Add new contact
+            </a>
+        </div>
+        <contacts-order></contacts-order>
     </div>
 @endsection
