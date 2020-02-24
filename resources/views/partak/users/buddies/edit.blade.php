fd7bef33d9d6b8870f0311341dc2ac942f49f1d3
Jan Zípek <jan@zipek.cz>
2020-02-24 22:03:45 +0100

    Fixed buddy&exchange student form

--- resources/views/partak/users/buddies/edit.blade.php
+++ resources/views/partak/users/buddies/edit.blade.php
@@ -2,24 +2,29 @@
 @section('inner-content')
 
     @if(session('successUpdate'))
-        <div class="row">
-            <div class="row-inner">
-                <div class="success">
-                    <i class="fas fa-check mr-1"></i> Profile was successfully updated.
-                </div>
-            </div>
+        <div class="success">
+            <i class="fas fa-check mr-1"></i> Profile was successfully updated.
         </div>
     @endif
 
 
     <div class="container">
-        <div class="row row-inner buddy-detail">
-            <img class="img-circle pull-left buddy-detail-img"  width="100" src="{{ asset($buddy->person->avatar()) }}">
-            <h3>{{ $buddy->person->first_name .' '. $buddy->person->last_name}}</h3>
-            @if($buddy->verified == 'y') <span class="glyphicon glyphicon-ok buddy-detail-icon"></span> Verified
-            @elseif ($buddy->verified == 'n') <span class="glyphicon glyphicon-time buddy-detail-icon"></span> Not verified yet
-            @else <span class="glyphicon glyphicon-remove buddy-detail-icon"></span> Denied
-            @endif
+        <div class="row container">
+            <div class="col-sm-2">
+                <img class="img-circle pull-left buddy-detail-img"  width="100" src="{{ asset($buddy->person->avatar()) }}">
+            </div>
+            <div class="col-sm-10">
+                <h3>{{ $buddy->person->first_name .' '. $buddy->person->last_name}}</h3>
+                <div class="info-line">
+                    @if($buddy->verified == 'y')
+                        <i class="fas fa-check fa-fw mr-1"></i> Verified
+                    @elseif ($buddy->verified == 'n')
+                        <i class="fas fa-hourglass-half fa-fw mr-1"></i> Not verified yet
+                    @else
+                        <i class="fas fa-times fa-fw mr-1"></i> Denied
+                    @endif
+                </div>
+            </div>
         </div>
     </div>
     <div class="container">
--- resources/views/partak/users/exchangeSudents/edit.blade.php
+++ resources/views/partak/users/exchangeSudents/edit.blade.php
@@ -1,68 +1,63 @@
 @extends('partak.users.layout')
 @section('inner-content')
-
     <div class="container">
         @if(session('successUpdate'))
-            <div class="row">
-                <div class="row-inner">
-                    <div class="success">
-                        <i class="fas fa-check mr-1"></i> Profile was successfully updated.
-                    </div>
-                </div>
+            <div class="success top-message">
+                <i class="fas fa-check mr-1"></i> Profile was successfully updated.
             </div>
         @endif
 
 
-        <div class="row">
-            <div class="row-inner">
+        <div class="row container">
+            <div class="col-sm-2">
                 <img class="img-circle pull-left buddy-detail-img"  width="125" src="{{ asset($exStudent->person->avatar()) }}">
-                <h3>{{ $exStudent->person->first_name .' '. $exStudent->person->last_name}}</h3>
-
+            </div>
+            <div class="col-sm-10">
+                <h3>{{ $exStudent->person->getFullName() }}</h3>
             </div>
         </div>
+
         <div class="row">
-            <div class="row-inner">
-                <div class="col-md-7">
-                    {{ Form::model($exStudent, ['url' => 'partak/users/exchange-students/edit/'. $exStudent->id_user, 'method' => 'patch']) }}
-                        {{ Form::bsText('email', 'Email','required', $exStudent->person->user->email) }}
-                        {{ Form::bsText('phone', 'Phone') }}
-                        {{ Form::bsTel('whatsapp', 'WhatsApp', '', null, [], 'Full number including the country prefix') }}
-                        {{ Form::bsUrl('facebook', 'Facebook', '', null, [], 'Full link to the Facebook profile') }}
-                        {{ Form::label('esn_registered', 'ESN registered', ['class' => 'control-label']) }}
-                        {{ Form::checkbox('esn_registered', 'y', $exStudent->esn_registered == 'y') }}
-                        {{ Form::bsText('esn_card_number', 'ESN card number') }}
-                        {{ Form::bsSelect('id_faculty', 'Faculty', $faculties, $exStudent->id_faculty, ['placeholder' => 'Choose faculty...', 'required' =>'required']) }}
-                        {{ Form::bsSelect('id_accommodation', 'Accommodation', $accommodations, $exStudent->id_accommodation, ['placeholder' => 'Choose accommodation...', 'required' =>'required']) }}
-                        {{ Form::bsSelect('id_country', 'Country', $countries, $exStudent->id_country, ['placeholder' => 'Choose country...', 'required' =>'required']) }}
+            <div class="col-md-7">
+                {{ Form::model($exStudent, ['url' => 'partak/users/exchange-students/edit/'. $exStudent->id_user, 'method' => 'patch']) }}
+                    {{ Form::bsText('email', 'Email','required', $exStudent->person->user->email) }}
+                    {{ Form::bsText('phone', 'Phone') }}
+                    {{ Form::bsTel('whatsapp', 'WhatsApp', '', null, [], 'Full number including the country prefix') }}
+                    {{ Form::bsUrl('facebook', 'Facebook', '', null, [], 'Full link to the Facebook profile') }}
+                    {{ Form::label('esn_registered', 'ESN registered', ['class' => 'control-label']) }}
+                    {{ Form::checkbox('esn_registered', 'y', $exStudent->esn_registered == 'y') }}
+                    {{ Form::bsText('esn_card_number', 'ESN card number') }}
+                    {{ Form::bsSelect('id_faculty', 'Faculty', $faculties, $exStudent->id_faculty, ['placeholder' => 'Choose faculty...', 'required' =>'required']) }}
+                    {{ Form::bsSelect('id_accommodation', 'Accommodation', $accommodations, $exStudent->id_accommodation, ['placeholder' => 'Choose accommodation...', 'required' =>'required']) }}
+                    {{ Form::bsSelect('id_country', 'Country', $countries, $exStudent->id_country, ['placeholder' => 'Choose country...', 'required' =>'required']) }}
 
-                        {{ Form::label('fullTime', 'Full-time student', ['class' => 'control-label']) }}
-                        {{ Form::checkbox('fullTime', 'y') }}
+                    {{ Form::label('fullTime', 'Full-time student', ['class' => 'control-label']) }}
+                    {{ Form::checkbox('fullTime', 'y') }}
 
-                        <div class="form-group row" style="overflow: hidden;">
-                            <div class="col-sm-6 left">
-                                {{ Form::label('sex', 'Sex', ['class' => 'control-label required']) }}
-                                @if ($errors->has('sex'))
-                                    <p class="error-block alert-danger">{{ $errors->first('sex') }}</p>
-                                @endif
-                                {{ Form::select('sex', ['M' => 'Male', 'F' => 'Female'], $exStudent->person->sex, ['placeholder' => 'Choose sex...', 'class' => 'form-control', 'required' =>'required']) }}
-                            </div>
-                            <div class="col-sm-6 right">
-                                {{ Form::label('age', 'Year of birth', ['class' => 'control-label']) }}
-                                @if ($errors->has('age'))
-                                    <p class="error-block alert-danger">{{ $errors->first('age') }}</p>
-                                @endif
-                                {{ Form::number('age', $exStudent->person->age, ['class' => 'form-control']) }}
-                            </div>
+                    <div class="form-group row" style="overflow: hidden;">
+                        <div class="col-sm-6 left">
+                            {{ Form::label('sex', 'Sex', ['class' => 'control-label required']) }}
+                            @if ($errors->has('sex'))
+                                <p class="error-block alert-danger">{{ $errors->first('sex') }}</p>
+                            @endif
+                            {{ Form::select('sex', ['M' => 'Male', 'F' => 'Female'], $exStudent->person->sex, ['placeholder' => 'Choose sex...', 'class' => 'form-control', 'required' =>'required']) }}
                         </div>
+                        <div class="col-sm-6 right">
+                            {{ Form::label('age', 'Year of birth', ['class' => 'control-label']) }}
+                            @if ($errors->has('age'))
+                                <p class="error-block alert-danger">{{ $errors->first('age') }}</p>
+                            @endif
+                            {{ Form::number('age', $exStudent->person->age, ['class' => 'form-control']) }}
+                        </div>
+                    </div>
 
-                        <!-- {{ Form::bsText('medical_issues', 'Medical issues','', $exStudent->person->medical_issues) }} -->
+                    <!-- {{ Form::bsText('medical_issues', 'Medical issues','', $exStudent->person->medical_issues) }} -->
 
-                        {{ Form::bsSelect('diet', 'Food preference', $diets, $exStudent->person->diet, ['placeholder' => 'No preference'])  }}
-                        {{ Form::bsTextarea('about', 'About') }}
-                        {{ Form::bsSubmit('Update profile') }}
+                    {{ Form::bsSelect('diet', 'Food preference', $diets, $exStudent->person->diet, ['placeholder' => 'No preference'])  }}
+                    {{ Form::bsTextarea('about', 'About') }}
+                    {{ Form::bsSubmit('Update profile') }}
 
-                    {{ Form::close() }}
-                </div>
+                {{ Form::close() }}
             </div>
         </div>
     </div>
