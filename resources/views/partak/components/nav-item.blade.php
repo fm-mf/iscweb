2047f8aee1bc4dab6afdf572b44afa3fdea5c5da
Jan Zípek <jan@zipek.cz>
2020-02-29 13:18:52 +0100

    Navigation is now properly expandable

--- resources/views/partak/components/nav-item.blade.php
+++ resources/views/partak/components/nav-item.blade.php
@@ -7,11 +7,19 @@
         Gate::check('acl', $acl)
     )
 )
-<li class="nav-item{{ strpos(request()->route()->getName(), $route) === 0 ? " active" : "" }}">
-    <a class="nav-link" href="{{ route($route) }}"><i class="fas @if($icon)fa-{{ $icon }}@endif fa-fw"></i> {{ $title }}</a>
+<?php $active = strpos(request()->route()->getName(), $route) === 0; ?>
+<?php $id = str_replace('.', '-', $route) ?>
+
+<li class="nav-item{{ $active ? " active" : "" }}">
+    @if (!$items)<a class="nav-link" href="{{ route($route) }}">@endif
+    @if ($items)<a class="nav-link" href="#{{ $id }}" role="button" data-toggle="collapse" aria-expanded="{{ $active ? 'true' : 'false' }}" aria-controls="{{ $id }}">@endif
+        <i class="fas @if($icon)fa-{{ $icon }}@endif fa-fw"></i> {{ $title }}
+    </a>
 
     @if ($items)
-        @include("partak.components.nav", ['item' => $items])
+        <div class="collapse{{ $active ? ' show' : ''}}" id="{{ $id }}">
+            @include("partak.components.nav", ['item' => $items])
+        </div>
     @endif
 </li>
 @endif
