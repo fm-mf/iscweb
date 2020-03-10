bd01f3aa7f75de2b5ddbf9794666d56dc794b719
Jan Zípek <jan@zipek.cz>
2020-03-10 18:39:11 +0100

    Updated buddies to verify screen

--- resources/views/partak/users/buddies/dashboard.blade.php
+++ resources/views/partak/users/buddies/dashboard.blade.php
@@ -12,26 +12,31 @@
         @endif
 
         @can('acl', 'buddy.verify')
-            <h3>Not verified Buddies</h3>
-            @if($notVerifiedBuddies->count() > 0)
-            <div class="table-responsive">
-                <table class="table p-table">
-                    @foreach($notVerifiedBuddies->get() as $buddy)
-                        <tr>
-                            <td>@include('partak.components.user-link', ['user' => $buddy->person])</td>
-                            <td>{{ $buddy->user()->email }}</td>
-                            <td align="right">
-                                <protectedbutton url="{{ url('partak/users/buddies/approve/' . $buddy->id_user) }}"
-                                                    protection-text="Approve buddy {{ $buddy->person->getFullName() }}?"
-                                                    button-style="btn-success btn-xs icon-button"><i class="fas fa-check mr-1"></i> Approve</protectedbutton>
-                                <protectedbutton url="{{ url('partak/users/buddies/deny/' . $buddy->id_user) }}"
-                                                    protection-text="Deny buddy {{ $buddy->person->getFullName() }}?"
-                                                    button-style="btn-danger btn-xs icon-button"><i class="fas fa-times mr-1"></i> Deny</protectedbutton>
-                            </td>
-                        </tr>
-                    @endforeach
-                </table>
-            </div>
+            <h3>Buddies to verify @if($notVerifiedBuddies->count() > 0)<span class="badge badge-warning">{{ $notVerifiedBuddies->count() }}</span>@endif</h3>
+
+            @if($notVerifiedBuddies->count() == 0)
+                <div class="text-center">
+                    <i class="fas fa-check text-success mr-2"></i> No buddies to verify
+                </div>
+            @else
+                <div class="table-responsive">
+                    <table class="table p-table">
+                        @foreach($notVerifiedBuddies->get() as $buddy)
+                            <tr>
+                                <td>@include('partak.components.user-link', ['user' => $buddy->person])</td>
+                                <td>{{ $buddy->user()->email }}</td>
+                                <td align="right">
+                                    <protectedbutton url="{{ url('partak/users/buddies/approve/' . $buddy->id_user) }}"
+                                                        protection-text="Approve buddy {{ $buddy->person->getFullName() }}?"
+                                                        button-style="btn-success btn-xs icon-button"><i class="fas fa-check mr-1"></i> Approve</protectedbutton>
+                                    <protectedbutton url="{{ url('partak/users/buddies/deny/' . $buddy->id_user) }}"
+                                                        protection-text="Deny buddy {{ $buddy->person->getFullName() }}?"
+                                                        button-style="btn-danger btn-xs icon-button"><i class="fas fa-times mr-1"></i> Deny</protectedbutton>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
             @endif
         @endcan
     </div>
