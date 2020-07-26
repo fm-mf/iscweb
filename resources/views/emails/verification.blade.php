0aac0536802284c14beb5ef91f70c5e893c2d375
Filip Marek <filip.marek.95@gmail.com>
2020-07-26 22:35:13 +0200

    Fix: Typo

--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -9,7 +9,7 @@
         <p>Děkujeme za registraci do Buddy Programu.</p>
         <p>Pro přístup do databáze zahraničních studentů klikni na následující odkaz:<br>
             <a href="{{ url('user/verify/' . $person->user->hash) }}">Ověřit</a>,<br>
-            případně zkopírojte do adresního řádku prohlížeče následující odkaz:<br>
+            případně zkopíruj do adresního řádku prohlížeče následující odkaz:<br>
             {{ url('user/verify/' . $person->user->hash) }}</p>
     </body>
 </html>
