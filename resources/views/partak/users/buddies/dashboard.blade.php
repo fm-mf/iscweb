f319b846c346257db1d6b923925b4eea35fe0359
Filip Marek <filip.marek.95@gmail.com>
2020-08-12 23:07:44 +0200

    Show motivation or university e-mail address in ParťákNet Buddies dashboard

--- resources/views/partak/users/buddies/dashboard.blade.php
+++ resources/views/partak/users/buddies/dashboard.blade.php
@@ -19,24 +19,37 @@
                     <i class="fas fa-check text-success mr-2"></i> No buddies to verify
                 </div>
             @else
-                <div class="table-responsive">
-                    <table class="table p-table">
-                        @foreach($notVerifiedBuddies->get() as $buddy)
-                            <tr>
-                                <td>@include('partak.components.user-link', ['user' => $buddy->person])</td>
-                                <td>{{ $buddy->user()->email }}</td>
-                                <td align="right">
-                                    <protectedbutton url="{{ url('partak/users/buddies/approve/' . $buddy->id_user) }}"
-                                                        protection-text="Approve buddy {{ $buddy->person->getFullName() }}?"
-                                                        button-style="btn-success btn-xs icon-button"><i class="fas fa-check mr-1"></i> Approve</protectedbutton>
-                                    <protectedbutton url="{{ url('partak/users/buddies/deny/' . $buddy->id_user) }}"
-                                                        protection-text="Deny buddy {{ $buddy->person->getFullName() }}?"
-                                                        button-style="btn-danger btn-xs icon-button"><i class="fas fa-times mr-1"></i> Deny</protectedbutton>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </table>
-                </div>
+                <ol class="table list-unstyled">
+                    <li class="row py-2 thead-dark bg-dark text-light">
+                        <div class="col-12 col-sm-4">Name</div>
+                        <div class="col-12 col-sm-4">E-mail</div>
+                        <div class="col-12 col-sm-4 text-right">Actions</div>
+                        <div class="col-12">Motivation / Verification e-mail</div>
+                    </li>
+                    @foreach($notVerifiedBuddies->get() as $buddy)
+                        <li class="row py-2">
+                            <div class="col-12 col-sm-4">@include('partak.components.user-link', ['user' => $buddy->person])</div>
+                            <div class="col-12 col-sm-4">{{ $buddy->user()->email }}</div>
+                            <div class="col-12 col-sm-4 text-right">
+                                <protectedbutton url="{{ url('partak/users/buddies/approve/' . $buddy->id_user) }}"
+                                                    protection-text="Approve buddy {{ $buddy->person->getFullName() }}?"
+                                                    button-style="btn-success btn-sm icon-button"><i class="fas fa-check mr-1"></i> Approve</protectedbutton>
+                                <protectedbutton url="{{ url('partak/users/buddies/deny/' . $buddy->id_user) }}"
+                                                    protection-text="Deny buddy {{ $buddy->person->getFullName() }}?"
+                                                    button-style="btn-danger btn-sm icon-button"><i class="fas fa-times mr-1"></i> Deny</protectedbutton>
+                            </div>
+                            <div class="col-12">
+                                @if($buddy->motivation)
+                                    <p class="mb-0"><strong>Motivation:</strong> {{ $buddy->motivation }}</p>
+                                @elseif(hash_equals($buddy->user()->email ?? "", $buddy->verification_email ?? ""))
+                                    <p class="mb-0">{{ $buddy->person->first_name }} has used university e-mail for registration</p>
+                                @else
+                                    <p class="mb-0">{{ $buddy->person->first_name }} has entered university e-mail for verification: {{ $buddy->verification_email }}</p>
+                                @endif
+                            </div>
+                        </li>
+                    @endforeach
+                </ol>
             @endif
         @endcan
     </div>
