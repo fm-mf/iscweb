341bc9588102eb21303dbc72ae59f7e86b58643b
Filip Marek <filip.marek.95@gmail.com>
2020-09-20 19:26:29 +0200

    Remove second class from the file
     - Replace Item class in AutocompleteController.php with AutocompleteItemResource class

---
+++ app/Http/Resources/AutocompleteItemResource.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Support\Collection;
+
+class AutocompleteItemResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $linkTemplate = $request->target;
+        if ($linkTemplate) {
+            preg_match('/{.*}/', $linkTemplate, $matches);
+            $targetColumn = substr($matches[0], 1, -1);
+            $targetColumnValue = $this->getColumnValue($this, $targetColumn);
+            $link = preg_replace('/{.*}/', $targetColumnValue, $linkTemplate);
+        }
+
+        return [
+            'topline' => $this->getLine($this, $request->topline),
+            'subline' => $this->getLine($this, $request->subline),
+            'link' => $link,
+            'image' => $this->person->avatar(),
+        ];
+    }
+
+    private function getLine($model, $fields)
+    {
+        if (empty($fields)) {
+            return '';
+        }
+
+        $values = Collection::make($fields)->map(function ($field) use ($model) {
+            return $this->getColumnValue($model, $field);
+        });
+
+        return $values->implode(' ');
+    }
+
+    private function getColumnValue($model, $field)
+    {
+        $path = explode('.', $field);
+        $cPath = $model;
+        foreach ($path as $t) {
+            $cPath = $cPath->$t;
+        }
+
+        return $cPath;
+    }
+}
