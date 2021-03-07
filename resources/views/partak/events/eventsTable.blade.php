c2b30b2b2e29f0ef883c059e365481fc136a774d
Filip Marek <filip.marek.95@gmail.com>
2021-03-07 23:12:57 +0100

    Fix text alignment in confirmation modal

--- resources/views/partak/events/eventsTable.blade.php
+++ resources/views/partak/events/eventsTable.blade.php
@@ -14,27 +14,29 @@
                 <td>{{ $event->name }}</td>
                 <td>{{ $event->datetime_from->toFormattedDateString() }}</td>
                 <td>{{ $event->visible_from->toFormattedDateString() }}</td>
-                <td class="text-right">
-                    @can('acl', 'events.edit')
-                        <a href="{{ route('partak.events.edit', $event) }}" class="btn btn-success btn-sm">
-                            <i class="fas fa-pen"></i> Edit
-                        </a>
-                    @endcan
-                    @can('acl', 'events.remove')
-                        {{ Form::open(['route' => ['partak.events.destroy', $event], 'method' => 'DELETE', 'class' => 'd-inline-block']) }}
-                            <protected-submit-button
-                                protection-title="Delete event?"
-                                protection-text="Do you really want to delete &quot;{{ $event->name }}&quot; event? The deletion irreversible!"
-                                proceed-text="Delete"
-                                classes="btn-danger btn-sm"
-                                proceed-classes="btn-danger"
-                                modal-id="delete-event-{{ $event->id_event }}"
-                                :form-group="false"
-                            >
-                                <span class="fas fa-trash"></span> Delete
-                            </protected-submit-button>
-                        {{ Form::close() }}
-                    @endcan
+                <td>
+                    <div class="d-flex justify-content-end">
+                        @can('acl', 'events.edit')
+                            <a href="{{ route('partak.events.edit', $event) }}" class="btn btn-success btn-sm">
+                                <i class="fas fa-pen"></i> Edit
+                            </a>
+                        @endcan
+                        @can('acl', 'events.remove')
+                            {{ Form::open(['route' => ['partak.events.destroy', $event], 'method' => 'DELETE', 'class' => 'd-inline-block ml-1']) }}
+                                <protected-submit-button
+                                    protection-title="Delete event?"
+                                    protection-text="Do you really want to delete &quot;{{ $event->name }}&quot; event? The deletion irreversible!"
+                                    proceed-text="Delete"
+                                    classes="btn-danger btn-sm"
+                                    proceed-classes="btn-danger"
+                                    modal-id="delete-event-{{ $event->id_event }}"
+                                    :form-group="false"
+                                >
+                                    <span class="fas fa-trash"></span> Delete
+                                </protected-submit-button>
+                            {{ Form::close() }}
+                        @endcan
+                    </div>
                 </td>
             </tr>
         @endforeach
