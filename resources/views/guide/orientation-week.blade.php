a89ffe2b81b4b72aaf9f65c18e7a8b0493b1c4eb
Filip Marek <filip.marek.95@gmail.com>
2021-07-13 02:21:02 +0200

    Update OW page in SG

--- resources/views/guide/orientation-week.blade.php
+++ resources/views/guide/orientation-week.blade.php
@@ -9,25 +9,30 @@
 
     <div class="row">
         <div class="col-md-12 bg-danger">
-            <p>Orientation Week is held by ISC one week before the semester starts and it provides a lot of important information
+            <p>
+                Orientation Week is held by ISC one week before the semester starts
+                and it provides you a lot of important information
                 which will help you to settle in the Czech Republic.
                 <strong>It is strongly recommended to attend this program.</strong>
                 During this week you will get to know the main bureaucratic procedures at CTU.
-                {{--
-                In addition, we will help you to make new local and international friends through parties, games and a lot of trips.
-                You shouldn’t miss this great opportunity!
-                --}}
+            </p>
+            <p>
+                In addition, we will help you to make new local and international friends
+                through parties, games and a lot of trips. You shouldn’t miss this great opportunity!
             </p>
         </div>
     </div>
 
     <div class="row">
         <div class="col-md-12 bg-warning">
-    {{--        <p>--}}
-    {{--            This is preliminary programme. Events may be moved to another days of the week. Details will be added during September.--}}
-    {{--        </p>--}}
             <p>
-                Due to the pandemic situation, the Orientation Week will be held online. We still want to provide you with a great experience and make it possible to meet each other. Definitive programme and detailed instructions will be provided on Monday during the presentation. You can also follow the <a href="{{ $linkOwFbEvent }}" target="_blank" rel="noopener">OW FB event</a> for up to date information.
+                This is preliminary programme. Events may be moved to another days of the week.
+                Details will be added during September.
+            </p>
+            <p>
+                Due to the pandemic situation, the Orientation Week might be held on-line.
+                We will try to provide you with a great experience and make it possible to meet each other.
+{{--                You can also follow the <a href="{{ $linkOwFbEvent }}" target="_blank" rel="noopener">OW FB event</a> for up to date information.--}}
             </p>
         </div>
     </div>
@@ -35,29 +40,29 @@
     
     @component('guide.section-nav', ['items' => [
         'day-1' => [
-            'title' => "Day 1 - $owDay1",
+            'title' => "Day 1 – $owDay1",
             'items' => [
                 'initial-meeting' => 'Initial information meeting',
-                'tandem-evening' => 'Tandem Evening'
+                'tandem-evening' => 'Tandem evening',
             ]
         ],
         'day-2' => [
-            'title' => "Day 2 - $owDay2",
+            'title' => "Day 2 – $owDay2",
             'items' => [
                 'day-2-registration' => 'Registration at faculties',
-                //'trips-meeting' => 'Second information meeting'
+                'trips-meeting' => 'Second information meeting',
             ]
         ],
         'day-3' => [
-            'title' => "Day 3 - $owDay3",
+            'title' => "Day 3 – $owDay3",
             'items' => [
                 // 'survival-czech-talk' => 'Survival Czech Talk',
-                'day-3-registration' => 'Prague Discovery Game',
-                'integreat-party' => 'inteGREATion party'
+                'prague-discovery-game' => 'Prague Discovery Game',
+                'integreation-party' => 'inteGREATion party',
             ]
         ],
         'day-4-7' => [
-            'title' => "Day 4-7 – $owTripsDays",
+            'title' => "Day 4–7 – $owTripsDays",
             'items' => [
                 'trips' => 'Trips around the Czech Republic'
             ]
@@ -65,13 +70,11 @@
     ]])
     @endcomponent
 
-{{--
     <div class="row">
         <div class="col-md-12 bg-info">
             <p><strong>The schedule of the Orientation Week will be announced soon. Stay tuned!</strong></p>
         </div>
     </div>
---}}
 
     <div class="row">
         <h2 id="day-1">DAY 1 – <strong>{{ $owDay1 }}</strong></h2>
@@ -80,27 +83,23 @@
         <div class="col-lg-12">
             <h3 id="initial-meeting">Initial information meeting</h3>
             <ul class="list-unstyled">
-                <li>When: <strong>13:00</strong> - 17:30</li>
                 {{--
+                <li>When: <strong>13:00</strong> - 17:30</li>
                 <li>Where: <a href="https://goo.gl/maps/wM3StYxDz8x3ipKx8" target="_blank"><strong>Bethelem Chappel</strong></a> (Betlémské nám. 255/4, 110 00 Staré Město)</li>
-                --}}
-                
-                <!--
+
                 <li>Meeting points:
                     <ul>
                         <li>13:00 at Strahov (bus stop Koleje Strahov)</li>
                         <li>13:20 in the lobby of the Masarykova dormitory</li>
                     </ul>
                 </li>
-                -->
+                --}}
 
                 <li>Topics:
                     <ul>
                         <li>Official welcome</li>
                         <li>How to enroll in courses and sports at CTU</li>
-                        {{--
                         <li>Czech language and Czech culture courses</li>
-                        --}}
                         <li>Life in the Czech Republic and how to survive in Prague</li>
                         <li>ESN membership</li>
                         <li>ISC teams and activities</li>
@@ -108,13 +107,13 @@
                         <li>Visa regulations</li>
                     </ul>
                 </li>
-		        <!--
+                {{--
                 <li>
                     <a class="btn btn-primary" href="{{ asset('files/ow-fall-2019/ow-fall-2019-complete.pdf') }}">
                         <span class="glyphicon glyphicon-file"></span> Download presentation
                     </a>
                 </li>
-                -->
+                --}}
             </ul>
         </div>
     </div>
@@ -123,12 +122,13 @@
             <h3 id="tandem-evening">Tandem Evening</h3>
             <p>A unique program for improving your language learning and teaching skills organized by ISC Languages team</p>
 
+            {{--
             <div class="bg-info">
                 <p>Due to the pandemic situation the Tandem Evening will be held online. Please follow the <a href="{{ $linkOwFbEvent }}" target="_blank" rel="noopener">OW FB event</a> for more info.</p>
             </div>
-            {{--
+            --}}
             <ul class="list-unstyled">
-                
+                {{--
                 <li>When: <strong>21:00</strong></li>
                 <li>Where: <a href="https://goo.gl/maps/ckJhJfZxaJuqnjfE7" target="_blank" rel="noopener"><strong>P.M. Club</strong> (Trojická 1907, 128 00 Nové Město)</a></li>
                 <li>Meeting points:
@@ -136,12 +136,13 @@
                         <li>20:00 in the lobby of the Masarykova dormitory</li>
                     </ul>
                 </li>
+                --}}
                 <li>See <a href="{{ url('activities/language-programs') }}" target="_blank">Language programs</a></li>
             </ul>
-            --}}
         </div>
     </div>
-    {{--<div class="row">
+    {{--
+    <div class="row">
         <div class="col-lg-12">
             <h3>OW presentation Monday</h3>
             <ul class="list-unstyled">
@@ -151,13 +152,12 @@
                 <li><a href="{{ asset('/files/ow-presentations-fall-2018/visa-fall-2018.pdf') }}" target="_blank">Visa</a></li>
                 <li><a href="{{ asset('/files/ow-presentations-fall-2018/isc-fall-2018.pdf') }}" target="_blank">ISC &amp; ESN</a></li>
                 <li><a href="{{ asset('/files/ow-presentations-fall-2018/culture-fatigue-fall-2018.pdf') }}" target="_blank">Culture Shock</a></li>
---}}{{--
                 <li><a href="{{ asset('/files/ow-presentations-fall-2018/esn-fall-2018.pdf') }}" target="_blank">ESN</a></li>
---}}{{--
                 <li><a href="{{ asset('/files/ow-presentations-fall-2017/kos-manual-ctu-fall-2017.pdf') }}" target="_blank">KOS manual</a></li>
             </ul>
         </div>
-    </div>--}}
+    </div>
+    --}}
 
     <div class="row">
         <h2 id="day-2">DAY 2 – {{ $owDay2 }}</h2>
@@ -166,11 +166,12 @@
         <div class="col-lg-12">
             <h3 id="day-2-registration">Registration at faculties</h3>
 
+            {{--
             <p>
                 Your faculty should contact you about the online registration and all the steps needed.
             </p>
+            --}}
 
-            {{--
             <p class="bg-warning">
                 Note: <strong>This is only for Erasmus students and Exchange students</strong>. Self-funding students who are studying for a CTU degree, and also Erasmus Mundus students, please contact Faculty coordinators at your study department.
             </p>
@@ -179,10 +180,10 @@
 
             <p class="bg-info">The schedule will be announced during September</p>
 
-            <ul class="list-unstyled">--}}
-                {{--<li>More information during the Monday presentation</li>--}}
+            {{--
+            <ul class="list-unstyled">
+                <li>More information during the Monday presentation</li>
 
-                {{--TBA:
                 <li><strong>When:</strong>
                     <ul>
                         <li>9:00 &ndash; <abbr title="Faculty of Transportation">FD</abbr>,
@@ -209,9 +210,8 @@
                 </li>
 
                 <li><abbr title="Faculty of Civil Engineering">FSv</abbr> has registrations on day 3</li>
-                --}}
 
-                {{--<li class="bg-danger"><strong>You will need:</strong>
+                <li class="bg-danger"><strong>You will need:</strong>
                     <ul>
                         <li>An ID (University ID / Passport)</li>
                         <li>A pen</li>
@@ -225,27 +225,29 @@
                         <li>Your email account</li>
                     </ul>
                 </li>
-            </ul>--}}
+            </ul>
+            --}}
         </div>
     </div>
 
-    {{--
     <div class="row">
         <div class="col-lg-12">
             <h3 id="trips-meeting">Second information meeting</h3>
+
             <ul class="list-unstyled">
+                {{--
                 <li>When: <strong>13:30</strong></li>
                 <li>Where: <strong><a href="https://goo.gl/maps/Lbp6SLLrovP2" target="_blank">Faculty of Mechanical Engineering</a>, room T4:D2-256</strong></li>
+                --}}
                 <li>Topics:
                     <ul>
                         <li>Presentation of ISC trips</li>
-                        <li>Registrations for trips organized for days 4&ndash;7 (<strong>ESNcard + cash needed</strong>)</li>
+                        <li>Registrations for trips organized for days 4–7 (<strong>ESNcard + cash needed</strong>)</li>
                     </ul>
                 </li>
             </ul>
         </div>
     </div>
-    --}}
 
     <div class="row">
         <h2 id="day-3">DAY 3 – {{ $owDay3 }}</h2>
@@ -274,16 +276,14 @@
 
             <p class="bg-info">The schedule will be announced during September</p>
 
-            {{--
             <p>When: <strong>10:00</strong> &ndash; <abbr title="Faculty of Civil Engineering">FSv</abbr></p>
         
             <li>Meeting points:
                 <ul>
                     <li>FSv &ndash; 9:50 in front of the faculty</li>
                 </ul>
-            </li>  
-            --}}   
-        {{--</div>
+            </li>
+        </div>
     </div>
     --}}
 
@@ -292,40 +292,39 @@
             <h3 id="prague-discovery-game">Prague Discovery Game</h3>
             <p>A great treasure hunt across Prague organized by the ISC Activities team</p>
 
+            {{--
             <p class="bg-info">
                 The exact time and other instructions will be posted in the <a href="{{ $linkOwFbEvent }}" target="_blank" rel="noopener">OW FB event</a> and during the Monday initial presentation.
             </p>
+            --}}
 
             {{--
             <ul class="list-unstyled">
                 <li>When: <strong>13:00</strong></li>
                 <li>Where: <strong><a href="https://goo.gl/maps/Lbp6SLLrovP2" target="_blank">Faculty of Mechanical Engineering</a>, room T4:D2-256</strong></li>
-                
             </ul>
             --}}
         </div>
     </div>
 
-    {{-- TBA
     <div class="row">
         <div class="col-lg-12">
-            <h3 id="integreat-party">inteGREATion party</h3>
+            <h3 id="integreation-party">inteGREATion party</h3>
             <p>Get to know each other at the very first ISC inteGREAT party and have fun!</p>
+            {{--
             <ul class="list-unstyled">
                 <li>When: <strong>21:00</strong></li>
                 <li>Where: <a href="https://goo.gl/maps/ebTnpr9oMqtAWj4a9" target="_blank" rel="noopener"><strong>Chapeau Rouge Bar</strong> (Jakubská 2, 110 00 Staré Město)</a></li>
-                <!--
                 <li>Meeting points:
                     <ul>
                         <li>19:30 at bus stop Koleje Strahov</li>
                         <li>19:30 in the lobby of the Masarykova dormitory</li>
                     </ul>
                 </li>
-                -->
             </ul>
+            --}}
         </div>
     </div>
-    --}}
 
     <div class="row">
         <h2 id="day-4-7">DAY 4–7 – {{ $owTripsDays }} </h2>
@@ -333,12 +332,11 @@
     <div class="row">
         <div class="col-lg-12">
             <h3 id="trips">Trips around the Czech Republic</h3>
-            
+            {{--
             <p>
                 Due to the pandemic situation and restrictions in Czech Republic we unfortunately can't organize trips for you as we planned. However we will provide you with some tips for trips to interesting places in Prague and nearby surroundings so you can decide on your own if you want to go and explore.
             </p>
-
-            {{--
+            --}}
             <p>One-day or two-day events introducing you to a lot of interesting places</p>
             <ul class="list-unstyled">
                 <li>When/Where: different times and places for each trip, further information on DAY 2 during the second information meeting</li>
@@ -350,7 +348,6 @@
                     </ul>
                 </li>
             </ul>
-            --}}
         </div>
     </div>
 
