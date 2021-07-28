1c34f7a659e3668cbd85e873959eb3adec157152
Filip Marek <filip.marek.95@gmail.com>
2021-07-28 22:21:38 +0200

    Move JS and Sass assets from assets subdirectory to resources to match Laravel 6 layout

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,64 +11,65 @@ const mix = require('laravel-mix');
  |
  */
 
-mix
-  .vue({ version: 2 })
-  .js('resources/assets/js/web.js', 'public/js')
-  .js('resources/assets/js/exchange-students-list.js', 'public/js')
-  .js('resources/assets/js/partak.js', 'public/js')
-  .js('resources/assets/js/reservation.js', 'public/js')
-  .js('resources/assets/js/partak-stats.js', 'public/js')
-  .js('resources/assets/js/partak-settings-hours.js', 'public/js')
-  .js('resources/assets/js/saf/saf.js', 'public/js/saf')
-  .js('resources/assets/js/tandem.js', 'public/js')
-  .js('resources/assets/js/ow-trips-stats.js', 'public/js')
-  .js('resources/assets/js/blog-rss-feed.js', 'public/js')
-  .js('resources/assets/js/printer.js', 'public/js')
-  .js('resources/assets/js/pickers.js', 'public/js')
-  .js('resources/assets/js/text-editor.js', 'public/js')
-  .js('resources/assets/js/crop-avatar.js', 'public/js')
-  .js('resources/assets/js/profile.js', 'public/js')
-  .sass('resources/assets/sass/web/web.scss', 'public/css')
-  .sass('resources/assets/sass/czech/czech.scss', 'public/css')
-  .sass('resources/assets/sass/czech/alumni.scss', 'public/css')
-  .sass('resources/assets/sass/auth/user.scss', 'public/css')
-  .sass('resources/assets/sass/auth/login.scss', 'public/css')
-  .sass('resources/assets/sass/partak/partaknet.scss', 'public/css')
-  .sass('resources/assets/sass/partak/pdf.scss', 'public/css')
-  .sass('resources/assets/sass/picker/picker.scss', 'public/css')
-  .sass('resources/assets/sass/buddyprogram/buddyprogram.scss', 'public/css')
-  .sass('resources/assets/sass/errors.scss', 'public/css')
-  .sass('resources/assets/sass/saf/saf.scss', 'public/css')
-  .sass('resources/assets/sass/saf/saf-partner.scss', 'public/css')
-  .sass('resources/assets/sass/tandem/tandem.scss', 'public/css')
-  .sass('resources/assets/sass/form/form.scss', 'public/css')
-  .sass('resources/assets/sass/components/avatar.scss', 'public/css')
-  .sass('resources/assets/sass/exchange-profile.scss', 'public/css')
-  .sass('resources/assets/sass/vendor.scss', 'public/css')
-  .copy(
-    'node_modules/@fortawesome/fontawesome-free/webfonts/*',
-    'public/fonts/font-awesome'
-  )
-  .copy('node_modules/cropperjs/src/images/*', 'public/img/cropper')
-  .copy('node_modules/jquery/dist/jquery.min.js', 'public/js')
-  .copy('node_modules/jquery/dist/jquery.slim.min.js', 'public/js')
-  .copy('node_modules/popper.js/dist/umd/popper.min.js', 'public/js')
-  .copy(
-    'node_modules/bootstrap/dist/js/bootstrap.min.js',
-    'public/js/bootstrap.4.min.js'
-  )
-  .copy(
-    'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
-    'public/js'
-  )
-  .copy(
-    'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-    'public/js'
-  )
-  .options({
-    processCssUrls: false
-  });
+mix.vue({ version: 2 })
+    .js('resources/js/web.js', 'public/js')
+    .js('resources/js/exchange-students-list.js', 'public/js')
+    .js('resources/js/partak.js', 'public/js')
+    .js('resources/js/reservation.js', 'public/js')
+    .js('resources/js/partak-stats.js', 'public/js')
+    .js('resources/js/partak-settings-hours.js', 'public/js')
+    .js('resources/js/saf/saf.js', 'public/js/saf')
+    .js('resources/js/tandem.js', 'public/js')
+    .js('resources/js/ow-trips-stats.js', 'public/js')
+    .js('resources/js/blog-rss-feed.js', 'public/js')
+    .js('resources/js/printer.js', 'public/js')
+    .js('resources/js/pickers.js', 'public/js')
+    .js('resources/js/text-editor.js', 'public/js')
+    .js('resources/js/crop-avatar.js', 'public/js')
+    .js('resources/js/profile.js', 'public/js')
+    .sass('resources/sass/web/web.scss', 'public/css')
+    .sass('resources/sass/czech/czech.scss', 'public/css')
+    .sass('resources/sass/czech/alumni.scss', 'public/css')
+    .sass('resources/sass/auth/user.scss', 'public/css')
+    .sass('resources/sass/auth/login.scss', 'public/css')
+    .sass('resources/sass/partak/partaknet.scss', 'public/css')
+    .sass('resources/sass/partak/pdf.scss', 'public/css')
+    .sass('resources/sass/picker/picker.scss', 'public/css')
+    .sass('resources/sass/buddyprogram/buddyprogram.scss', 'public/css')
+    .sass('resources/sass/errors.scss', 'public/css')
+    .sass('resources/sass/saf/saf.scss', 'public/css')
+    .sass('resources/sass/saf/saf-partner.scss', 'public/css')
+    .sass('resources/sass/tandem/tandem.scss', 'public/css')
+    .sass('resources/sass/form/form.scss', 'public/css')
+    .sass('resources/sass/components/avatar.scss', 'public/css')
+    .sass('resources/sass/exchange-profile.scss', 'public/css')
+    .sass('resources/sass/vendor.scss', 'public/css')
+    .copy(
+        'node_modules/@fortawesome/fontawesome-free/webfonts/*',
+        'public/fonts/font-awesome'
+    )
+    .copy('node_modules/cropperjs/src/images/*', 'public/img/cropper')
+    .copy('node_modules/jquery/dist/jquery.min.js', 'public/js')
+    .copy('node_modules/jquery/dist/jquery.slim.min.js', 'public/js')
+    .copy('node_modules/popper.js/dist/umd/popper.min.js', 'public/js')
+    .copy(
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'public/js/bootstrap.4.min.js'
+    )
+    .copy(
+        'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
+        'public/js'
+    )
+    .copy(
+        'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+        'public/js'
+    )
+    .options({
+        processCssUrls: false
+    });
 
 if (!mix.inProduction()) {
-  mix.sourceMaps();
+    mix.sourceMaps();
+} else {
+    mix.version();
 }
