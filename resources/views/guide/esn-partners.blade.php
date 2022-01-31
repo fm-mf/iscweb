ce18af150c749d6d7947894202fa83aa12d3f9b8
Filip Marek <filip.marek.95@gmail.com>
2022-01-31 14:24:00 +0100

    [Guide] Update ESN partners

--- resources/views/guide/esn-partners.blade.php
+++ resources/views/guide/esn-partners.blade.php
@@ -19,27 +19,36 @@
         <h3>Vodafone</h3>
         <img src="{{ asset('img/guide/partners/vodafone.png') }}" alt="Vodafone logo" class="square-logo" />
         <p>
-            Stay in touch with your Erasmus friends and obtain a SIMcard with a favorable
+            Stay in touch with your Erasmus friends and obtain a SIM card with a favorable
             Erasmus student tariff together with an ISC membership! First month free with 3 GB data
-            and free SMS to Vodafone. Then you get a 50% discount, so it's just 174 CZK (about € 7).
+            and free SMS to Vodafone. Then you get a 50% discount, so it's just 174.50 CZK (about € 7).
         </p>
+        <p class="mb-0">
+            <strong>Every month you can activate in the app My Vodafone additional 5 GB of data for a total of 8 GB / month.</strong>
+            The My Vodafone app is available for
+        </p>
+        <ul>
+            <li class="mb-0"><a href="https://play.google.com/store/apps/details?id=com.zentity.vodafone" target="_blank" rel="noopener">Android (Google Play)</a></li>
+            <li class="mb-0"><a href="https://itunes.apple.com/cz/app/muj-vodafone/id509838162?mt=8" target="_blank" rel="noopener">iOS (App Store)</a></li>
+            <li class="mb-0"><a href="https://appgallery.huawei.com/#/app/C102006481" target="_blank" rel="noopener">Huawei (App Gallery)</a></li>
+        </ul>
         <p>
             <a href="https://www.esncz.org/partners/vodafone" target="_blank" rel="noopener">
-                www.esncz.org/partners/vodafone
+                More information about the Vodafone partnership
             </a>
         </p>
         <p>
-            Don't forget that you should have enough balance on your account. Otherwise you will
-            not be able to pay 174 CZK after each 30 days and the discount will be lost.
+            Do not forget that you should have enough balance on your account. Otherwise you will
+            not be able to pay 174.50 CZK after each 30 days and the discount will be lost.
         </p>
-        <p>
+        <p class="mb-0">
             How to top up your account? You have several options:
         </p>
         <ul>
-            <li>Tobacco shops (with Sazka terminal)</li>
-            <li>ATM machines (works with foreign debit card)</li>
-            <li>Online (works with Czech debit card)</li>
-            <li>Vodafone shops</li>
+            <li class="mb-0">Tobacco shops (with Sazka terminal)</li>
+            <li class="mb-0">ATM machines (works with foreign debit card)</li>
+            <li class="mb-0">Online (works with Czech debit card)</li>
+            <li class="mb-0">Vodafone shops</li>
         </ul>
 
         <h3>SmartGuide</h3>
@@ -56,7 +65,7 @@
 
         <h3>Ryanair</h3>
         <img src="{{ asset('img/guide/partners/ryanair.svg') }}" alt="Ryanair logo" />
-        <p><a href="https://esn.org/ryanair-faq" target="_blank" rel="noopener">https://esn.org/ryanair-faq</a></p>
+        <p><a href="https://esn.org/ryanair-faq" target="_blank" rel="noopener">Ryanair partnership FAQ</a></p>
         <p>
             An exclusive partnership was launched in 2017 between the largest European airline,
             Ryanair, and the Erasmus Student Network.
@@ -93,28 +102,48 @@
             FlixBus was able to establish Europe's largest intercity bus network in the shortest amount of time.
         </p>
         <p><a href="https://esncard.org/flixbus" target="_blank" rel="noopener">Enjoy 10% discount with your voucher.</a></p>
-        <p>*You can claim up to 12 vouchers per ESNcard.</p>
+        <p>* You can claim up to 12 vouchers per ESNcard.</p>
 
-        {{--
-        <h3>Revolut</h3>
-        <img src="{{ asset('img/guide/partners/revolut.svg') }}" alt="Revolut logo" />
+        <h3>Uniqway</h3>
+        <img src="{{ asset('img/guide/partners/uniqway.svg') }}" alt="Uniqway logo" />
         <p>
-            Revolut is an online bank that provides its customers with
-            <strong>free accounts, free currency exchange, and free payment cards</strong>.
-            Revolut card can be used all over the world to pay and withdraw money without
-            any fees (note: banks can have some fees e.g. Česká spořitelna). Revolut has
-            an app that allows you to send money to other Revolut account within seconds
-            and for free, so it's perfect for managing your expenses with your friends.
+            Uniqway is carsharing made by students of ČVUT, ČZU and VŠE for students and employees
+            of all universities in the Czech Republic (Exchange students included).
+            In our fleet there are many different cars - ŠKODA FABIA, ŠKODA SCALA, electric ŠKODA CITIGOᵉ iV
+            and ŠKODA ENYAQ iV. Many of them have automatic transitions.
         </p>
         <p>
-            If you are still not sure what Revolut is, take a look at
-            <a href="https://www.youtube.com/watch?v=QabM_PvBtsI">this short video</a>
-            or read through <a href="https://www.revolut.com/">their website</a>.
+            To enjoy a 200 CZK discount with your ESNcard, register your ESNcard on
+            <a href="https://esncard.org" target="_blank" rel="noopener">esncard.org</a>
+            and generate your discount code. Enter the code when registering for Uniqway.
         </p>
         <p>
-            Use <a href="https://revolut.ngih.net/kGWO3">this link</a> to order the Revolut card for FREE!
+            No ESNcard? You can still enjoy a 100 CZK discount with our code <strong>ESNCZ</strong>. (Note: You can only use one of these codes.)
         </p>
-        --}}
+        <p>
+            <a href="https://esncz.org/partners/uniqway">More information about the Uniqway partnership</a>
+        </p>
+
+        <h3>HoppyGo</h3>
+        <img src="{{ asset('img/guide/partners/hoppygo.png') }}" alt="HoppyGo logo" />
+        <p>
+            Take advantage of the unique discounts of the HoppyGo platform
+            and rent your dream car for a summer trip easily, flexibly and without unnecessary paperwork.
+        </p>
+        <p>
+            You can choose from 2 500 cars of various brands and categories for any occasion.
+            Cars are also available in our city. You can find the complete offer on the
+            <a href="https://hoppygo.com/" target="_blank" rel="noopener">HoppyGo platform website</a>.
+        </p>
+        <p>
+            When booking a car, enter the code <strong>ISCCTU</strong> and get a CZK 200 discount on rent.
+            The car is taken over using the smart mobile application HoppyGo
+            (<a href="https://play.google.com/store/apps/details?id=cz.nom.smilecarweb" target="_blank" rel="noopener">Google Play</a>,
+            <a href="https://itunes.apple.com/cz/app/hoppygo/id1184613465?mt=8" target="_blank" rel="noopener">App Store</a>).
+        </p>
+        <p>
+            The discount applies to car rental in the Czech Republic.
+        </p>
 
         <h3>Grammarly</h3>
         <img src="{{ asset('img/guide/partners/grammarly.jpg') }}" alt="Grammarly logo" />
@@ -156,15 +185,19 @@
         <h3>My Conscious Space</h3>
         <img src="{{ asset('img/guide/partners/my_conscious_space.png') }}" alt="My Conscious Space logo" />
         <p>
-            My Conscious Space creates a safe online container for your self-development and healing. We offer a space with guidance, where we support each other on our self-discovery journey. Everything is reachable online from the warmth of your home. Choose what suits you the best: yoga, meditation, breathing techniques, deep relaxation, women circles or full/new moon ceremonies. All the events are LIVE, using Zoom platform.
+            My Conscious Space creates a safe online container for your self-development and healing.
+            We offer a space with guidance, where we support each other on our self-discovery journey.
+            Everything is reachable online from the warmth of your home. Choose what suits you the best:
+            yoga, meditation, breathing techniques, deep relaxation, women circles or full/new moon ceremonies.
+            All the events are LIVE, using Zoom platform.
         </p>
         <p><a href="https://myconscious.space/isc/" target="_blank" rel="noopener">Enjoy Three Weeks of Unlimited Yoga for 15 euro.</a></p>
 
         <h3>Loď Pivovar</h3>
         <img src="{{ asset('img/guide/partners/lod_pivovar.png') }}" alt="Loď Pivovar logo" />
         <p>
-            Restaurant with its own brewery, where you can enjoy Czech cuisine, beer garden and summer terrace with bar, BBC and relaxing lounge.
-            All this awaits you on three decks of a unique project in Czech waters.
+            Restaurant with its own brewery, where you can enjoy Czech cuisine, beer garden and summer terrace with bar,
+            BBC and relaxing lounge. All this awaits you on three decks of a unique project in Czech waters.
         </p>
         <p>Check it out at <a href="https://www.lodpivovar.cz/" target="_blank">www.lodpivovar.cz</a></p>
         <p>Upon showing your ESNcard you'll get <strong>5% discount</strong> on beer.</p>
