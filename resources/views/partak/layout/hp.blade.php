5cf30df903bb161ac480bb5b399b44a1bcdd81bf
Filip Marek <filip.marek.95@gmail.com>
2022-10-05 14:45:20 +0200

    [ParťákNet] Update dashboard photo with the new Team retreat group

--- resources/views/partak/layout/hp.blade.php
+++ resources/views/partak/layout/hp.blade.php
@@ -2,8 +2,8 @@
 @section('page')
     <div class="dashboard-header">
         <picture>
-            <source type="image/webp" srcset="{{ asset('img/partak/team-photo.webp') }}" />
-            <img src="{{ asset('img/partak/team-photo.jpg') }}" alt="Team photo" />
+            <source type="image/webp" srcset="{{ asset('img/partak/team_photo.webp') }}" />
+            <img src="{{ asset('img/partak/team_photo.jpg') }}" alt="Team photo" />
         </picture>
     </div>
 
