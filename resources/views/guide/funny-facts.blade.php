0089de85c417d20d2ad81e581fa7c2ca624f112f
tomaspycha <37267608+tomaspycha@users.noreply.github.com>
2022-10-10 09:46:07 +0200

    Update funny-facts.blade.php (#279)
    Aktualizace po diskusi na BW

--- resources/views/guide/funny-facts.blade.php
+++ resources/views/guide/funny-facts.blade.php
@@ -7,15 +7,15 @@
     <p><strong>10 exchange students' commandments</strong></p>
     <ol>
         <li>Learn how to do ISC fishing.</li>
-        <li>Visit Vzorkovna at least once a month</li>
-        <li>Go to Chapeau Rouge. Why? We don’t know but everyone does!</li>
+        <li>Visit Scout Institute at least once a month</li>
+        <li>Visit the Invisible Exhibition. Why? We don’t know but everyone does!</li>
         <li>Go to Klubovna once a week.</li>
-        <li>Do not miss any inteGREAT party!</li>
+        <li>Do not miss any Culture Evening!</li>
         {{--
         <li>Spend every other evening in bar downstairs in Masarykova</li>
         --}}
         <li>Try things you would never do home.</li>
-        <li>Sing Wonderwall!</li>
+        <li>Get a photo of yourself on the Red Couch in the ISC Point!</li>
         <li>Smile at Czechs!</li>
         <li>Do a Freedom run across Charles Bridge</li>
         <li>What happens on Erasmus, stays on Erasmus.</li>
