d85dd48e093197b3e75a892bcfa17fb5ff8f60ae
Mebillot <117483663+Mebillot@users.noreply.github.com>
2023-02-17 19:20:01 +0100

    Mebillot photo update (#293)
    * [PartakNet] Update of team photo
    
    Updated team photo, old files need to be replaced
    
    * [PartakNet] Fixed size of the header picture
    
    Fixed previously wrong sized header
    
    * [PartakNet] Size change of WEBP file
    
    * Add another Team photo and logic for selecting and showing one of the two Team photos
    
    ---------
    
    Co-authored-by: Filip Marek <filip.marek.95@gmail.com>

--- resources/views/partak/layout/hp.blade.php
+++ resources/views/partak/layout/hp.blade.php
@@ -1,9 +1,18 @@
 @extends('partak.layout.main')
+
+@php
+    $photoPaths = [
+        'img/partak/team_photo-0',
+        'img/partak/team_photo-1',
+    ];
+    $photoPath = $photoPaths[rand(1,100) % 2];
+@endphp
+
 @section('page')
     <div class="dashboard-header">
         <picture>
-            <source type="image/webp" srcset="{{ asset('img/partak/team_photo.webp') }}" />
-            <img src="{{ asset('img/partak/team_photo.jpg') }}" alt="Team photo" />
+            <source type="image/webp" srcset="{{ asset("$photoPath.webp") }}" />
+            <img src="{{ asset("$photoPath.jpg") }}" alt="Team photo" />
         </picture>
     </div>
 
