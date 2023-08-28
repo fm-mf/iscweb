a32454146dd9466b4c96715ec00e43d90d855680
Filip Marek <filip.marek.95@gmail.com>
2023-08-28 20:50:47 +0200

    Fix Typeform for new Buddies (#304)
    * Update Typeform code
    
    * Add English version of the Typeform

--- resources/views/auth/verification/_typeform.blade.php
+++ resources/views/auth/verification/_typeform.blade.php
@@ -1,11 +1,14 @@
-<!-- Change the width and height values to suit you best -->
-<div class="typeform-widget" data-url="https://iscctu.typeform.com/to/c2edVv" data-text="Buddy Registration" style="width:100%;height:500px;"></div>
+@php
+    $formIdCzech = 'c2edVv';
+    $formIdEnglish = 'ghFtdC9m';
+    $formId = app()->getLocale() === 'cs' ? $formIdCzech : $formIdEnglish;
+@endphp
 
-<div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;">Powered by<a href="https://www.typeform.com/examples/?utm_campaign=c2edVv&amp;utm_source=typeform.com-58650-Basic&amp;utm_medium=typeform&amp;utm_content=typeform-embedded-poweredbytypeform&amp;utm_term=CS" style="color: #999" target="_blank">Typeform</a></div>
+<!-- Typeform -->
+<div data-tf-widget="{{ $formId }}"  data-tf-opacity="75" data-tf-disable-scroll data-tf-auto-resize id="form" class="typeform-widget" style="min-height: 55ex"></div>
 
 @section('scripts')
     @parent
-    <script>
-        (function(){ var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm', b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){ js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q) } })()
-    </script>
+    <script src="https://embed.typeform.com/next/embed.js" defer="defer"></script>
 @endsection
+<!-- /Typeform -->
